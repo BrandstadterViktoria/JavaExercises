91b54bb76fca6b7c810309499eb70eee8d217752
BrandstadterViktoria <nunviktoria@gmail.com>
2017-07-28 15:44:11 +0200

    Create an OrderLexicographically class
    The class needs String userinput and an int userinput and substrings the given String with the given int

---
+++ JavaPracticingBasics/Strings/String2/src/OrderLexicographically.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class OrderLexicographically {
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+
+        return smallest + "\n" + largest;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+        for (int i = 0; i <= s.length() - k; i++) {
+            String substr = s.substring(i, i + k);
+            {
+                System.out.println(substr);
+            }
+        }
+
+
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
+
+
