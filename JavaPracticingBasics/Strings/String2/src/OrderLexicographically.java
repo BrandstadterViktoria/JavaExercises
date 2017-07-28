dee42193610da2eb329b1f87c73962836c9013d3
BrandstadterViktoria <nunviktoria@gmail.com>
2017-07-28 15:48:02 +0200

    Delete the line comment

--- JavaPracticingBasics/Strings/String2/src/OrderLexicographically.java
+++ JavaPracticingBasics/Strings/String2/src/OrderLexicographically.java
@@ -5,11 +5,6 @@ public class OrderLexicographically {
     public static String getSmallestAndLargest(String s, int k) {
         String smallest = "";
         String largest = "";
-
-        // Complete the function
-        // 'smallest' must be the lexicographically smallest substring of length 'k'
-        // 'largest' must be the lexicographically largest substring of length 'k'
-
         return smallest + "\n" + largest;
     }
 
