22edf822bf93aed22f23876ad0a42e3104706a73
BrandstadterViktoria <nunviktoria@gmail.com>
2017-07-28 12:41:34 +0200

    Create a main method which operate with uppercase and the length of the String

---
+++ JavaPracticingBasics/Strings/src/UppercaseSubstringLenght.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class UppercaseSubstringLenght {
+    public static void main(String[] args) {
+        System.out.println("Type me strings");
+        Scanner sc = new Scanner(System.in);
+        String A = sc.nextLine();
+        String B = sc.nextLine();
+        int lenght = A.length() + B.length();
+        char[] chars = {Character.toUpperCase(A.charAt(0)),
+                Character.toUpperCase(B.charAt(0))};
+        String a1 = chars[0] + A.substring(1);
+        String b1 = chars[1] + B.substring(1);
+        System.out.println(lenght);
+        System.out.println(a1 + " " + b1);
+    }
+}
+
