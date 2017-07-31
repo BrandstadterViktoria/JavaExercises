5b3118d3c40b1cd36fc70d97686e22d42ad1e582
BrandstadterViktoria <nunviktoria@gmail.com>
2017-07-31 16:50:31 +0200

    Change the Arraylist in the main method to a 2D ArrayList
    The next task is to get the specify elements of the arraylist with located in yth and xth position.

--- JavaPracticingBasics/Arrays/ArrayList/src/ArrayListPractice.java
+++ JavaPracticingBasics/Arrays/ArrayList/src/ArrayListPractice.java
@@ -1,19 +1,19 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class ArrayListPractice {
 
     public static void main(String[] args) {
+        List<List<Integer>> array = new ArrayList<List<Integer>>();
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         for (int i = 0; i < n; i++) {
             int d = scan.nextInt();
-            ArrayList<Integer> list = new ArrayList<>(d);
-            for (int k = 0; k < d; i++) {
-                int val = scan.nextInt();
-                list.add(val);
-//
+            List<Integer> list = new ArrayList<Integer>();
+            array.add(list);
+            for (int j = 0; j < d; j++) {
+                list.add(scan.nextInt());
             }
-            scan.close();
         }
     }
 }
