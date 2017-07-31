65525a6924b58a8d2d8b77dc35f3a30dcddcf1a9
BrandstadterViktoria <nunviktoria@gmail.com>
2017-07-31 14:37:20 +0200

    Adds two for loops to sum negative subarrays

--- JavaPracticingBasics/Arrays/ArraysSortNegativeSubarrays/src/ArraySort.java
+++ JavaPracticingBasics/Arrays/ArraysSortNegativeSubarrays/src/ArraySort.java
@@ -1,14 +1,26 @@
 import java.util.Scanner;
 
-
 public class ArraySort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] n = new int[];
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int count = 0;
         for (int i = 0; i < n; i++) {
             int val = sc.nextInt();
-            n[i] = val;
+            a[i] = val;
         }
+        sc.close();
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum = a[j] + sum;
+                if (sum < 0) {
+                    count ++;
+                }
+            }
+        }
+        System.out.println(count);
 
     }
 }
