0294bd5b91171dd82c2a11a6da1fb833df8fe603
BrandstadterViktoria <nunviktoria@gmail.com>
2017-08-01 15:28:40 +0200

    Add the conditionals to the main method of IfElsePractice1
    Conditionals were the following:f  is odd, print Weird
    •If  is even and in the inclusive range of  to , print Not Weird
    •If  is even and in the inclusive range of  to , print Weird
    •If  is even and greater than , print Not Weird
    •If  is odd, print Weird

--- JavaPracticingBasics/If-else & Loops/src/IfElsePractice1.java
+++ JavaPracticingBasics/If-else & Loops/src/IfElsePractice1.java
@@ -3,18 +3,20 @@ import java.util.Scanner;
 public class IfElsePractice1 {
     public static void main(String[] args) {
 
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        String ans="";
-        if(n%2==1){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String ans = "";
+        if (n % 2 == 1) {
             ans = "Weird";
+        } else if (n < 5 && n > 2)
+            ans = "Not Weird";
+        else if (n < 20 && n > 6)
+            ans = "Weird";
+        else if (n > 20)
+            ans = "Not Weird";
+        {
+            System.out.println(ans);
         }
-        else{
-
-            //Complete the code
-
-        }
-        System.out.println(ans);
 
     }
 }
