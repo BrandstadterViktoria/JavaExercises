fb9dfd97a22b402ca52ac91bed7bfe42eabf82a4
BrandstadterViktoria <nunviktoria@gmail.com>
2017-08-01 14:35:33 +0200

    Delete the comments
    Delete all the line comments, since they arent needed anymore.

--- JavaPracticingBasics/Arrays/ArrayListJoinTwoLists/src/JoinTwoArrayLists.java
+++ JavaPracticingBasics/Arrays/ArrayListJoinTwoLists/src/JoinTwoArrayLists.java
@@ -6,8 +6,8 @@ public class JoinTwoArrayLists {
     public static void main(String[] args) {
         ArrayList<String> girls = new ArrayList<String>(Arrays.asList("Eve", "Ashley", "Bözsi", "Kat", "Jane"));
         ArrayList<String> boys = new ArrayList<String>(Arrays.asList("Joe", "Fred", "Béla", "Todd", "Neef", "Jeff"));
-        ArrayList<String> order = new ArrayList<String>(girls.size()); // Make a new list
-        for (int i = 0; i < girls.size(); i++) { // Loop through every name/phone number combo
+        ArrayList<String> order = new ArrayList<String>(girls.size()); 
+        for (int i = 0; i < girls.size(); i++) {
             order.add(girls.get(i) + " " + boys.get(i));
         }
         System.out.print(order);
