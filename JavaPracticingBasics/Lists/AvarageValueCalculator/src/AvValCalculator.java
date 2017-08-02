2609dafe9559c191e32375141c92c0ca5e6fe4f0
BrandstadterViktoria <nunviktoria@gmail.com>
2017-08-02 11:07:54 +0200

    Create the main method and the method AvValCalculator
    Mehtod createthe list which should be manipulate

---
+++ JavaPracticingBasics/Lists/AvarageValueCalculator/src/AvValCalculator.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AvValCalculator {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(7);
+        numbers.add(9);
+        numbers.add(5);
+
+        AvValCalculator(numbers);
+
+    }
+
+    private static double AvValCalculator(List<Integer> numbers) {
+        
+    }
+}
