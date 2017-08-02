4ea6a258288cafe4981593624975839ec2d07795
BrandstadterViktoria <nunviktoria@gmail.com>
2017-08-02 12:25:12 +0200

    Create the printUsage method in case of no arguments are given in the command line.

--- JavaPracticingBasics/MultipleTasks/UseCommandLine/src/FileCopy.java
+++ JavaPracticingBasics/MultipleTasks/UseCommandLine/src/FileCopy.java
@@ -1,4 +1,34 @@
+import java.util.Objects;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 public class FileCopy {
+    public static void main(String[] args) {
+        try {
+            if (Objects.equals(args[0], " ")) {
+                printUsage();
+            }
+            if (Objects.equals(args[0], "-a")) {
+                addNewTask(args[1]);
+            }
+            if (args[0].equals("-c")) {
+                checkTask(Integer.parseInt(args[1]));
+            }
+            if (args[0].equals("-r")) {
+                removeTask(Integer.parseInt(args[1]));
+            }
+        }catch (IllegalArgumentException ex){
+            argumentErrorHandling();
+        }
+    }
 
+    public static void printUsage() {
+        System.out.println(" copy [source] [destination]");
 }
+
+
