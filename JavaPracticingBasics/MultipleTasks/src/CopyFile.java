d57d5452b21f87b0eb47b89261ed0b3a2b17637a
BrandstadterViktoria <nunviktoria@gmail.com>
2017-08-03 13:13:22 +0200

    Create the main method command line arguments
    Create in the main method in if and else if structure the conditions needed.

--- JavaPracticingBasics/MultipleTasks/src/CopyFile.java
+++ JavaPracticingBasics/MultipleTasks/src/CopyFile.java
@@ -1,3 +1,18 @@
 
 public class CopyFile {
-}
+
+    public class FileCopy {
+        public static void main(String[] args) {
+            if (args.length == 0) {
+                printUsage();
+            }
+            else if (for (String a : args)){
+            }
+            else if  (args.length < 2) {
+           }
+        }
+    }
+    private static void printUsage() {
+
+    }
+    }
