4890037e08eb7200553830168e1431ed620e225b
BrandstadterViktoria <nunviktoria@gmail.com>
2017-08-03 14:00:38 +0200

    Rename the method printUsage and create the method printWithOneArgument
    The printUsage name didnt express correctly the function, so senaming was necessary. Moreover the change defines the method  printWithOneArgument wit a simply sout.

--- JavaPracticingBasics/MultipleTasks/src/CopyFile.java
+++ JavaPracticingBasics/MultipleTasks/src/CopyFile.java
@@ -7,19 +7,24 @@ public class CopyFile {
 
         public static void main(String[] args) {
             if (args.length == 0) {
-                printUsage();
+                printWithoutArgument();
             } else if (args.length < 2){
+                printWithOneArgument();
 
             } else if (args.length == 3){
                 copy(args[1], args[2]);
             }
             }
 
-    private static void printUsage() {
+    private static void printWithoutArgument() {
         System.out.println(" copy [source] [destination]");
     }
 
-    public static void copy(String source, String destination) {
+    private static void printWithOneArgument (){
+        System.out.println("No destination provided");
+    }
+
+    private static void copy(String source, String destination) {
         try {
             Files.copy(Paths.get(source), Paths.get(destination), StandardCopyOption.REPLACE_EXISTING);
 
