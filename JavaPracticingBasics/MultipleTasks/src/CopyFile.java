a3a95819202fed9fc185373be643583a93ef61c0
BrandstadterViktoria <nunviktoria@gmail.com>
2017-08-03 13:36:01 +0200

    Change the order of the else if statement, and specify the args.declaration
    After the change in the order, modifie the second else if statement, as a result if the dest. and resource filename is given in command line, the method will call the copy method.

--- JavaPracticingBasics/MultipleTasks/src/CopyFile.java
+++ JavaPracticingBasics/MultipleTasks/src/CopyFile.java
@@ -1,18 +1,30 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class CopyFile {
 
-    public class FileCopy {
         public static void main(String[] args) {
             if (args.length == 0) {
                 printUsage();
+            } else if (args.length < 2){
+
+            } else if (args.length == 3){
+                copy(args[1], args[2]);
             }
-            else if (for (String a : args)){
             }
-            else if  (args.length < 2) {
-           }
+
+    private static void printUsage() {
+        System.out.println(" copy [source] [destination]");
+    }
+
+    public static void copy(String source, String destination) {
+        try {
+            Files.copy(Paths.get(source), Paths.get(destination), StandardCopyOption.REPLACE_EXISTING);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
-    private static void printUsage() {
-
-    }
-    }
+ }
