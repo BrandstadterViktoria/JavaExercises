0253be3d8922fafa99fdfe80da83acafc2ab9655
BrandstadterViktoria <nunviktoria@gmail.com>
2017-08-04 16:45:15 +0200

    Define the method total, with a loop which sum the elements

--- JavaPracticingBasics/BasicJUnitTests/Test2/src/Total.java
+++ JavaPracticingBasics/BasicJUnitTests/Test2/src/Total.java
@@ -12,7 +12,11 @@ public class Total {
    }
 
    public int total (ArrayList <Integer>integers){
-
+       int total = 0;
+       for(int i = 1; i < integers.size(); i++){
+           total += integers.get(i);
+       }
+       return total;
     }
 
 }
