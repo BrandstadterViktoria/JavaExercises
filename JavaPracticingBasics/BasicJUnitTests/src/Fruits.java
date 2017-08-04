3194c990e11df2a4ddb24520cd9f3173fa426d97
BrandstadterViktoria <nunviktoria@gmail.com>
2017-08-04 15:55:30 +0200

    Create the constructor and initalize the String fruits

--- JavaPracticingBasics/BasicJUnitTests/src/Fruits.java
+++ JavaPracticingBasics/BasicJUnitTests/src/Fruits.java
@@ -1,6 +1,12 @@
 
 public class Fruits {
 
+    private String fruits;
+
+    public Fruits (String fruits){
+        this.fruits = fruits;
+    }
+
     public String getFruits (){
         String fruits = "fruit";
         return fruits;
---
+++ JavaPracticingBasics/BasicJUnitTests/tests/FruitsTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FruitsTest {
+    @Test
+    public void getFruits() throws Exception {
+    }
+
+}
