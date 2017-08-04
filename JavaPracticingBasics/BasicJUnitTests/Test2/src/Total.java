2812508c316cd2d836660a50648be929cb407ab3
BrandstadterViktoria <nunviktoria@gmail.com>
2017-08-04 16:52:07 +0200

    Create the test class

--- JavaPracticingBasics/BasicJUnitTests/Test2/src/Total.java
+++ JavaPracticingBasics/BasicJUnitTests/Test2/src/Total.java
@@ -3,12 +3,8 @@ import java.util.ArrayList;
 public class Total {
    private ArrayList<Integer> integers;
 
-   public Total(ArrayList <Integer> integers){
+   public Total(){
        integers = new ArrayList <>();
-       for (int i = 1; i <10 ; i++){
-           integers.add(i);
-       }
-       this.integers=integers;
    }
 
    public int total (ArrayList <Integer>integers){
---
+++ JavaPracticingBasics/BasicJUnitTests/Test2/tests/TotalTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TotalTest {
+    @Test
+    public void total() throws Exception {
+        Total testTotal = new Total();
+    }
+
+}
