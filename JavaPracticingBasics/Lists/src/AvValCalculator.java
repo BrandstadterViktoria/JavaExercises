eba2cc5258a02699cc60b83ef27217b9e5eb91f2
BrandstadterViktoria <nunviktoria@gmail.com>
2017-08-04 14:15:54 +0200

    Modifie the main method, now the List numberList is renamed,and redefined
    List has a new name numberList and is defined by a for loop, and in the avarageValueCalculator method the parameter ha alo new name nemgerListForUse

---
+++ JavaPracticingBasics/Lists/src/AvValCalculator.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AvValCalculator {
+
+    public static void main(String[] args) {
+        List<Integer> numberList = new ArrayList<>();
+        for (int i = 1; i < 10; i++) {
+            numberList.add(i);
+        }
+    }
+
+    private static double avarageValueCalculator(List<Integer> numberListForUse) {
+        int sum = 0;
+        int numberOdds = 0;
+        for (int i = 0; i < numberListForUse.size(); i++) {
+            if (numberListForUse.get(i) % 2 == 1) {
+                numberOdds++;
+                sum += numberListForUse.get(i);
+
+            }
+            System.out.print(sum / numberOdds);
+        }
+        return (sum / numberOdds);
+    }
+}
