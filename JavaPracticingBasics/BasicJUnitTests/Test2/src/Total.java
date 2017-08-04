eae712be97b55155fea9932d4a847ef2561301f7
BrandstadterViktoria <nunviktoria@gmail.com>
2017-08-04 16:40:39 +0200

    Create the Total class for testing
    Define the constructor and the ArrayList parameter filling it by a for loop.Create the method body of total.

---
+++ JavaPracticingBasics/BasicJUnitTests/Test2/src/Total.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Total {
+   private ArrayList<Integer> integers;
+
+   public Total(ArrayList <Integer> integers){
+       integers = new ArrayList <>();
+       for (int i = 1; i <10 ; i++){
+           integers.add(i);
+       }
+       this.integers=integers;
+   }
+
+   public int total (ArrayList <Integer>integers){
+
+    }
+
+}
