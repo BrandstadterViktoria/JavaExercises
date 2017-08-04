a20b386cc1b399bd9dc6b40313b4bd9b8784c244
BrandstadterViktoria <nunviktoria@gmail.com>
2017-08-04 17:03:30 +0200

    First testcase, tested with assertequals

--- JavaPracticingBasics/BasicJUnitTests/Test2/tests/TotalTest.java
+++ JavaPracticingBasics/BasicJUnitTests/Test2/tests/TotalTest.java
@@ -1,11 +1,19 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class TotalTest {
     @Test
     public void total() throws Exception {
         Total testTotal = new Total();
+        ArrayList <Integer> testList = new ArrayList<>();
+        for (int i = 1; i <10; i++) {
+            testList.add(i);
+        }
+        assertEquals(44,testTotal.total(testList));
     }
 
 }
