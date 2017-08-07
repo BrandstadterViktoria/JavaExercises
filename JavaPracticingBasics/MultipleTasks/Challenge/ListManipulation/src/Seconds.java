2fbc480ee3f7b2545c42e5158d176fa53cd34de9
BrandstadterViktoria <nunviktoria@gmail.com>
2017-08-07 13:09:13 +0200

    Create Seconds class
    the class contains a main method and a setListWithSecondElements method, which returns a list with all the second elements of its parameter List.

---
+++ JavaPracticingBasics/MultipleTasks/Challenge/ListManipulation/src/Seconds.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Seconds {
+    public static void main(String[] args) {
+        List<Integer> numberList = new ArrayList<>();
+        for (int i = 1; i < 10; i++) {
+            numberList.add(i);
+        }
+        System.out.println(setListWithSecondElements(numberList));
+    }
+
+    public static List<Integer> setListWithSecondElements(List<Integer> numberListForUse){
+        List<Integer> secondElements = new ArrayList<>();
+        for (int i = 0; i < numberListForUse.size(); i+=2) {
+            secondElements.add(i);
+        }
+        return secondElements;
+    }
+}
