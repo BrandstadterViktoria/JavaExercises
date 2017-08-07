e43bca0cadcea7d83cef49b4598233f4c6f0115f
BrandstadterViktoria <nunviktoria@gmail.com>
2017-08-07 13:43:08 +0200

    Adds readAlllines method

--- JavaPracticingBasics/MultipleTasks/Challenge/FileManipulation/src/CountAs.java
+++ JavaPracticingBasics/MultipleTasks/Challenge/FileManipulation/src/CountAs.java
@@ -1,13 +1,24 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class CountAs {
     public static void main(String[] args) {
 
+
     }
 
-    public int countAsInFile(){
+    public int countAsInFile(String myFile) {
         int numberAs;
+        myFile = "./resources/TextFileCountAs";
+        try {
+            Path filePath = Paths.get(myFile);
+            List<String> lines = Files.readAllLines(filePath);
+            
 
-
+        } catch (Exception e) {
+        }
         return numberAs;
     }
 
