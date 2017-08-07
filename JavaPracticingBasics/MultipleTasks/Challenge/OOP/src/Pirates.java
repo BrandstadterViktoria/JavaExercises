e951f936bfac6d77283a79f682a52801657f9fc8
BrandstadterViktoria <nunviktoria@gmail.com>
2017-08-07 16:45:44 +0200

    Add the list if name of the pirates

--- JavaPracticingBasics/MultipleTasks/Challenge/OOP/src/Pirates.java
+++ JavaPracticingBasics/MultipleTasks/Challenge/OOP/src/Pirates.java
@@ -14,43 +14,50 @@ public class Pirates {
             this.gold = gold;
         }
 
-        public static int getGold(){
+        public static int getGold() {
             return gold;
         }
 
-        public String getName(){
-            return this.name;
+        public static String getName() {
+            return name;
         }
 
         public static boolean gethashWoodenLeg() {
-           return hashWoodenLeg;
+            return hashWoodenLeg;
 
         }
     }
-        public static void main(String[] args) {
-            ArrayList<Pirate> pirates = new ArrayList<>();
-            Pirate pirate1 = (new Pirate("Olaf", false, 12));
-            Pirate pirate2 = (new Pirate("Uwe", true, 9));
-            Pirate pirate3 = (new Pirate("Jack", true, 16));
-            Pirate pirate4 = (new Pirate("Morgan", false, 17));
-            Pirate pirate5 = (new Pirate("Hook", true, 20));
 
-            pirates.add(pirate1);
-            pirates.add(pirate2);
-            pirates.add(pirate3);
-            pirates.add(pirate4);
-            pirates.add(pirate5);
+    public static void main(String[] args) {
+        ArrayList<Pirate> pirates = new ArrayList<>();
+        Pirate pirate1 = (new Pirate("Olaf", false, 12));
+        Pirate pirate2 = (new Pirate("Uwe", true, 9));
+        Pirate pirate3 = (new Pirate("Jack", true, 16));
+        Pirate pirate4 = (new Pirate("Morgan", false, 17));
+        Pirate pirate5 = (new Pirate("Hook", true, 20));
 
+        pirates.add(pirate1);
+        pirates.add(pirate2);
+        pirates.add(pirate3);
+        pirates.add(pirate4);
+        pirates.add(pirate5);
+
+        pirateCounter(pirates);
+
+    }
+
+    public static ArrayList<String> pirateCounter(ArrayList<Pirate> inputPirates) {
+        ArrayList<String> pirateNames = new ArrayList<>();
+        ArrayList<Pirate> countedPirates = new ArrayList<>();
+        for (int i = 0; i < inputPirates.size(); i++) {
+            if (Pirate.gethashWoodenLeg() && (Pirate.getGold() < 15)) {
+                countedPirates.add(inputPirates.get(i));
+            }
         }
-
-    public static ArrayList<Pirate> pirateCounter(ArrayList<Pirate> inputPirates) {
-            ArrayList<Pirate> countedPirates = new ArrayList<>();
-           for (int i = 0; i < inputPirates.size(); i++) {
-               if (Pirate.gethashWoodenLeg() && (Pirate.getGold() < 15)) {
-                   countedPirates.add(inputPirates.get(i));
-               }
-           }
-             return countedPirates;
-
+            for (int j = 0; j < countedPirates.size(); j++) {
+                String names = Pirate.getName();
+                pirateNames.add(names);
+            }
+        return pirateNames;
     }
 }
