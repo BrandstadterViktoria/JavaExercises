7efe67fd64bd18dbe0a7a3ae1b8932a188688011
BrandstadterViktoria <nunviktoria@gmail.com>
2017-08-07 15:01:04 +0200

    Gives the conditions in the method PirateCounter

--- JavaPracticingBasics/MultipleTasks/Challenge/OOP/src/Pirates.java
+++ JavaPracticingBasics/MultipleTasks/Challenge/OOP/src/Pirates.java
@@ -3,9 +3,9 @@ import java.util.*;
 public class Pirates {
 
     static class Pirate {
-        String name;
-        boolean hashWoodenLeg;
-        int gold;
+        static String name;
+        static boolean hashWoodenLeg;
+        static int gold;
 
         Pirate(String name, boolean hashWoodenLeg, int gold) {
             this.name = name;
@@ -14,7 +14,7 @@ public class Pirates {
         }
     }
 
-    public static void main (String [] args){
+    public static void main(String[] args) {
         ArrayList<Pirate> pirates = new ArrayList<>();
         pirates.add(new Pirate("Olaf", false, 12));
         pirates.add(new Pirate("Uwe", true, 9));
@@ -22,5 +22,18 @@ public class Pirates {
         pirates.add(new Pirate("Morgan", false, 17));
         pirates.add(new Pirate("Hook", true, 20));
 
+        pirateCounter(pirates);
+
+    }
+
+    public static ArrayList<Pirate> pirateCounter(ArrayList<Pirate> inputPirates) {
+        ArrayList<Pirate> countedPirates = new ArrayList<>();
+        for (int i = 0; i < inputPirates.size(); i++) {
+            if (Pirate.hashWoodenLeg = true && Pirate.gold < 15) {
+                countedPirates.add(inputPirates.get(i));
+            }
+        }
+        System.out.println(countedPirates);
+        return countedPirates;
     }
 }
