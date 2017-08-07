8ad7febde16dda4fcfcf6d9202715aaf9d687c6a
BrandstadterViktoria <nunviktoria@gmail.com>
2017-08-07 15:54:16 +0200

    Add Compare Object to the class Pirate

--- JavaPracticingBasics/MultipleTasks/Challenge/OOP/src/Pirates.java
+++ JavaPracticingBasics/MultipleTasks/Challenge/OOP/src/Pirates.java
@@ -1,38 +1,63 @@
 import java.util.*;
 
+
 public class Pirates {
 
     static class Pirate {
-        static String name;
-        static boolean hashWoodenLeg;
-        static int gold;
+        String name;
+        boolean hashWoodenLeg;
+        int gold;
 
         Pirate(String name, boolean hashWoodenLeg, int gold) {
             this.name = name;
             this.hashWoodenLeg = hashWoodenLeg;
             this.gold = gold;
         }
-    }
 
-    public static void main(String[] args) {
-        ArrayList<Pirate> pirates = new ArrayList<>();
-        pirates.add(new Pirate("Olaf", false, 12));
-        pirates.add(new Pirate("Uwe", true, 9));
-        pirates.add(new Pirate("Jack", true, 16));
-        pirates.add(new Pirate("Morgan", false, 17));
-        pirates.add(new Pirate("Hook", true, 20));
+        public boolean getHashWoodenLeg() {
+            return this.hashWoodenLeg;
+        }
 
-        pirateCounter(pirates);
+        public int getGold(){
+            return this.gold;
+        }
 
-    }
+        public String getName(){
+            return this.name;
+        }
 
-    public static ArrayList<Pirate> pirateCounter(ArrayList<Pirate> inputPirates) {
-        ArrayList<Pirate> countedPirates = new ArrayList<>();
-        for (int i = 0; i < inputPirates.size(); i++) {
-            if (Pirate.hashWoodenLeg = true && Pirate.gold < 15) {
-                countedPirates.add(inputPirates.get(i));
+        static class PirateComparator implements Comparator<Pirate> {
+
+            @Override
+            public int compare(Pirate pirate1, Pirate pirate2) {
+                int pirate1
+                return 0;
             }
         }
-        return countedPirates;
+    }
+        public static void main(String[] args) {
+            ArrayList<Pirate> pirates = new ArrayList<>();
+            Pirate pirate1 = (new Pirate("Olaf", false, 12));
+            Pirate pirate2 = (new Pirate("Uwe", true, 9));
+            Pirate pirate3 = (new Pirate("Jack", true, 16));
+            Pirate pirate4 = (new Pirate("Morgan", false, 17));
+            Pirate pirate5 = (new Pirate("Hook", true, 20));
+
+            pirates.add(pirate1);
+            pirates.add(pirate2);
+            pirates.add(pirate3);
+            pirates.add(pirate4);
+            pirates.add(pirate5);
+
+        }
+
+        public List<String> pirateCounter(ArrayList<Pirate> inputPirates) {
+        ArrayList <String> sortedPiratesByName = new ArrayList<>();
+            Collections.sort(inputPirates, new Pirate.PirateComparator());
+            for (Pirate sortedPirates : inputPirates){
+                sortedPiratesByName = new ArrayList<String>(Arrays.asList(sortedPirates.getName().split(",")));
+                System.out.println(sortedPirates.getName());
+        }
+        return sortedPiratesByName;
     }
 }
