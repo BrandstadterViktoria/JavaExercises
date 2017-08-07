b920660b280be6d4f231c9b86f935b5d7824e638
BrandstadterViktoria <nunviktoria@gmail.com>
2017-08-07 13:35:15 +0200

    Creates the CountAs class with main and countAsInFile methods
    also adds a TextFileCountAs txt. file for use.

---
+++ JavaPracticingBasics/MultipleTasks/Challenge/FileManipulation/src/CountAs.java
@@ -0,0 +1,15 @@
+
+public class CountAs {
+    public static void main(String[] args) {
+
+    }
+
+    public int countAsInFile(){
+        int numberAs;
+
+
+        return numberAs;
+    }
+
+
+}
