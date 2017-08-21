7eae495d3f74ba053a7433ed4dbdb5dedf08733d
BrandstadterViktoria <nunviktoria@gmail.com>
2017-08-21 16:40:34 +0200

    Create the displayHIghScorePosition method

--- JavaPracticingBasics/If-else & Loops/HighScorePosition/src/HoghScorePosition.java
+++ JavaPracticingBasics/If-else & Loops/HighScorePosition/src/HoghScorePosition.java
@@ -4,6 +4,10 @@ public class HoghScorePosition {
 
     }
 
+    public static void displayHighScorePosition(String playername, int position) {
+        System.out.println(playername + " managed to get into position " + position + " on the high score table");
+
+    }
     public static int calculateHighScorePosition(int playerScore) {
         if (playerScore > 1000) {
             int position = 1;
