ce902dbec04187c7c459eb79992ab3c5f5984647
BrandstadterViktoria <nunviktoria@gmail.com>
2017-08-21 16:37:23 +0200

    Create
    Create the calculateHighScorePosition method, with the conditions.

---
+++ JavaPracticingBasics/If-else & Loops/HighScorePosition/src/HoghScorePosition.java
@@ -0,0 +1,25 @@
+
+public class HoghScorePosition {
+    public static void main(String[] args) {
+
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if (playerScore > 1000) {
+            int position = 1;
+            return position;
+        }
+        if ((playerScore > 500 && playerScore < 1000)){
+            int position = 2;
+            return position;
+        }
+        if (playerScore > 100 && playerScore < 500){
+            int position = 3;
+            return position;
+        }
+        else{int position = 4;
+            return position;
+        }
+    }
+
+}
