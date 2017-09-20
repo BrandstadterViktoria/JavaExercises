27043d6740d9312ca33b37fb391006d08883952f
BrandstadterViktoria <nunviktoria@gmail.com>
2017-09-20 11:43:05 +0200

    Creates the main method and the isAngram method

--- javapracticingbasics/Strings/Anagram/src/Anagram.java
+++ javapracticingbasics/Strings/Anagram/src/Anagram.java
@@ -1,6 +1,15 @@
 public class Anagram {
 
+    public boolean isAnagram(String a, String b) {
+        boolean isAnagram;
+
+
+
+        return isAnagram;
+    }
+
     public static void main(String[] args) {
+
 
     }
 
