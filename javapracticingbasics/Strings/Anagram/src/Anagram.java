85f6359cfe493048b551a4e657514d644718ece3
BrandstadterViktoria <nunviktoria@gmail.com>
2017-09-20 11:46:07 +0200

    Changes Strings to char arrays

--- javapracticingbasics/Strings/Anagram/src/Anagram.java
+++ javapracticingbasics/Strings/Anagram/src/Anagram.java
@@ -2,8 +2,8 @@ public class Anagram {
 
     public boolean isAnagram(String a, String b) {
         boolean isAnagram;
-
-
+        char[] word1 = a.replaceAll("[\\s]", "").toCharArray();
+        char[] word2 = b.replaceAll("[\\s]", "").toCharArray();
 
         return isAnagram;
     }
