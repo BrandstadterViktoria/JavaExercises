c0d8d20892465c79f14978bc33735cb8fd49e13d
BrandstadterViktoria <nunviktoria@gmail.com>
2017-09-20 11:59:22 +0200

    Defines the Arrays.sort method and gives conditions to the isAnagram method
    In order to return the proper boolean, dependong on wther the String a and b are anagrams or not.

--- javapracticingbasics/Strings/Anagram/src/Anagram.java
+++ javapracticingbasics/Strings/Anagram/src/Anagram.java
@@ -1,11 +1,18 @@
+import java.util.Arrays;
+
 public class Anagram {
 
     public boolean isAnagram(String a, String b) {
         boolean isAnagram;
         char[] word1 = a.replaceAll("[\\s]", "").toCharArray();
         char[] word2 = b.replaceAll("[\\s]", "").toCharArray();
-
-        return isAnagram;
+        Arrays.sort(word1);
+        Arrays.sort(word2);
+        if (Arrays.equals(word1, word2)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static void main(String[] args) {
