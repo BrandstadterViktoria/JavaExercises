b486b6d5a33e74a11360a56dd71b10aa186cd547
BrandstadterViktoria <nunviktoria@gmail.com>
2017-09-25 14:15:10 +0200

    Define the two elements which sum is the target variable

--- JavaPracticingBasics/Arrays/ArrayReturnIndex/src/ArrayReturnIndex.java
+++ JavaPracticingBasics/Arrays/ArrayReturnIndex/src/ArrayReturnIndex.java
@@ -1,7 +1,30 @@
+import java.util.Arrays;
+
 public class ArrayReturnIndex {
 
+    public static void main(String[] args) {
+        int [] nums = {2, 13, 4, 15, 5};
+        int target = 6;
+    }
+
     public int[] twoSum(int[] nums, int target) {
-        
+        int [] indexOfSummedNumbers = new int [2];
+        int arrayElement1 = 0;
+        int arrayElement2 =0;
+        for (int i = 0; i <nums.length ; i++) {
+            do {
+                 arrayElement2 = target - nums[i];
+            }
+            while (!(Arrays.asList(nums).contains(arrayElement2) ));
+        }
+        arrayElement1 = target - arrayElement2;
+        int indexElement1 = Arrays.asList(nums).indexOf(arrayElement1);
+        int indexElement2 = Arrays.asList(nums).indexOf(arrayElement2);
+
+
+        return indexOfSummedNumbers;
+
     }
 
     }
+
