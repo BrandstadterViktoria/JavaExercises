d752b9e1aa0b29fdec640cf6171f8745ad04468c
BrandstadterViktoria <nunviktoria@gmail.com>
2017-10-02 12:21:49 +0200

    Create the SortIntegers method with bubble sort program

--- JavaPracticingBasics/Arrays/ArraySortedDescendingOrder/src/SortingArrayDescendingOrder.java
+++ JavaPracticingBasics/Arrays/ArraySortedDescendingOrder/src/SortingArrayDescendingOrder.java
@@ -1,14 +1,13 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class SortingArrayDescendingOrder {
 
     public static void main(String[] args) {
-
         printArray(getIntegers());
 
     }
 
-    public static int [] getIntegers () {
+    public static int[] getIntegers() {
         Scanner scanner = new Scanner(System.in);
         int[] myArray = new int[7];
         for (int i = 0; i < myArray.length; i++) {
@@ -17,7 +16,21 @@ public class SortingArrayDescendingOrder {
         return myArray;
     }
 
-    public static void printArray(int [] array){
+    public static void printArray(int[] array) {
         System.out.println(array);
+    }
+
+    public static int[] sortArray(int[] array) {
+        int[] sortedArray = new int[7];
+        for (int i = 0; i < (array.length - 1); i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] < array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return sortedArray;
     }
 }
