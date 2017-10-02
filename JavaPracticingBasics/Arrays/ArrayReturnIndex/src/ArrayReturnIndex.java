f42c840a375b065564df52ba3ce2373610640f64
BrandstadterViktoria <nunviktoria@gmail.com>
2017-10-02 11:37:49 +0200

    Create the class SortingArrayDescendingOrder with psvm

--- JavaPracticingBasics/Arrays/ArrayReturnIndex/src/ArrayReturnIndex.java
+++ JavaPracticingBasics/Arrays/ArrayReturnIndex/src/ArrayReturnIndex.java
@@ -7,9 +7,11 @@ public class ArrayReturnIndex {
     public static void main(String[] args) {
         int [] nums = {2, 13, 4, 15, 5};
         int target = 6;
+
+        System.out.println(twoSum(nums,target));
     }
 
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
         List<Integer> indexOfSummedNumbers = new ArrayList<>();
         int arrayElement1 = 0;
         int arrayElement2 =0;
@@ -18,15 +20,17 @@ public class ArrayReturnIndex {
                  arrayElement2 = target - nums[i];
             }
             while (!(Arrays.asList(nums).contains(arrayElement2) ));
+            
         }
+
         arrayElement1 = target - arrayElement2;
         int indexElement1 = Arrays.asList(nums).indexOf(arrayElement1);
         int indexElement2 = Arrays.asList(nums).indexOf(arrayElement2);
         indexOfSummedNumbers.add(indexElement1);
         indexOfSummedNumbers.add(indexElement2);
+        System.out.println(indexOfSummedNumbers.toString());
         int [] ints = indexOfSummedNumbers.stream().mapToInt(Integer::intValue).toArray();
         return ints;
-
     }
 
     }
---
+++ JavaPracticingBasics/Arrays/ArraySortedDescendingOrder/src/SortingArrayDescendingOrder.java
@@ -0,0 +1,5 @@
+public class SortingArrayDescendingOrder {
+    public static void main(String[] args) {
+        
+    }
+}
