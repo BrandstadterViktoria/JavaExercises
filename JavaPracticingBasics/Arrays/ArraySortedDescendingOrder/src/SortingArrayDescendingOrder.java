9579198409e6758dc11dcd81aa9cb6d4c7c34e47
BrandstadterViktoria <nunviktoria@gmail.com>
2017-10-02 11:50:39 +0200

    Create getIntegers method
    in order to get the sytem input to fill the array.

--- JavaPracticingBasics/Arrays/ArraySortedDescendingOrder/src/SortingArrayDescendingOrder.java
+++ JavaPracticingBasics/Arrays/ArraySortedDescendingOrder/src/SortingArrayDescendingOrder.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class SortingArrayDescendingOrder {
+
     public static void main(String[] args) {
-        
+
+    }
+
+    public int [] getIntegers () {
+        Scanner scanner = new Scanner(System.in);
+        int[] myArray = new int[7];
+        for (int i = 0; i < myArray.length; i++) {
+            myArray[i] = scanner.nextInt();
+        }
+        return myArray;
     }
 }
