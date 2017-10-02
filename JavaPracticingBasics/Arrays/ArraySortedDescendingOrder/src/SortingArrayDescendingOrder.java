a67ecc14003561b2ca704f59a34f02e042f3e804
BrandstadterViktoria <nunviktoria@gmail.com>
2017-10-02 11:52:58 +0200

    Create printArray method

--- JavaPracticingBasics/Arrays/ArraySortedDescendingOrder/src/SortingArrayDescendingOrder.java
+++ JavaPracticingBasics/Arrays/ArraySortedDescendingOrder/src/SortingArrayDescendingOrder.java
@@ -4,14 +4,20 @@ public class SortingArrayDescendingOrder {
 
     public static void main(String[] args) {
 
+        printArray(getIntegers());
+
     }
 
-    public int [] getIntegers () {
+    public static int [] getIntegers () {
         Scanner scanner = new Scanner(System.in);
         int[] myArray = new int[7];
         for (int i = 0; i < myArray.length; i++) {
             myArray[i] = scanner.nextInt();
         }
         return myArray;
+    }
+
+    public static void printArray(int [] array){
+        System.out.println(array);
     }
 }
