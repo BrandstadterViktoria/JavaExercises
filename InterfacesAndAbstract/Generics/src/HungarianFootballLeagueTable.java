402837a3812eb62bf1f4509d3e86605cb206de1f
BrandstadterViktoria <nunviktoria@gmail.com>
2017-10-04 16:05:43 +0200

    Supplement the Team class with the needed only one String, setters-getters and Constructor.

--- InterfacesAndAbstract/Generics/src/HungarianFootballLeagueTable.java
+++ InterfacesAndAbstract/Generics/src/HungarianFootballLeagueTable.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 public class HungarianFootballLeagueTable < T extends Team>{
+    private int score;
+    private int numberOfWins;
 
     private ArrayList<T> memmberOfTheLeague = new ArrayList<>();
 
