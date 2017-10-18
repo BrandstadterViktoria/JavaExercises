1512154047ca529200c2d3fa0670175089a40cab
BrandstadterViktoria <nunviktoria@gmail.com>
2017-10-18 14:36:12 +0200

    Create psvm in the main class and move the viki object there

--- InterfacesAndAbstract/InterfacesPractice1/src/Main.java
+++ InterfacesAndAbstract/InterfacesPractice1/src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
 
-    public static void saveObject(Saver objectSaver) {
-        Players viki = new Players("Viki", "female","BVik",32);
+    public static void main(String[] args) {
+        Players viki = new Players("Viki", "female", "BVik", 32);
         System.out.println(viki.toString());
-        for (int i = 0; i <objectSaver.writeTheValues().size(); i++) {
-            System.out.println("Saved" + objectSaver.writeTheValues().get(i) +"to my device");
+    }
+
+    public static void saveObject(Saver objectSaver) {
+        for (int i = 0; i < objectSaver.writeTheValues().size(); i++) {
+            System.out.println("Saved" + objectSaver.writeTheValues().get(i) + "to my device");
         }
     }
 }
