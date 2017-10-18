f356001bf7fd87fe82f49fac32ad135440886bbe
BrandstadterViktoria <nunviktoria@gmail.com>
2017-10-18 14:42:09 +0200

    In psvm call saveOject method

--- InterfacesAndAbstract/InterfacesPractice1/src/Main.java
+++ InterfacesAndAbstract/InterfacesPractice1/src/Main.java
@@ -3,11 +3,18 @@ public class Main {
     public static void main(String[] args) {
         Players viki = new Players("Viki", "female", "BVik", 32);
         System.out.println(viki.toString());
+
+        viki.setGender("male");
+        viki.setName("Clark");
+        viki.setHitPoints(89);
+        saveObject(viki);
+        saveObject(viki);
+
     }
 
     public static void saveObject(Saver objectSaver) {
         for (int i = 0; i < objectSaver.writeTheValues().size(); i++) {
-            System.out.println("Saved" + objectSaver.writeTheValues().get(i) + "to my device");
+            System.out.println("Saved " + objectSaver.writeTheValues().get(i) + " to my device");
         }
     }
 }
