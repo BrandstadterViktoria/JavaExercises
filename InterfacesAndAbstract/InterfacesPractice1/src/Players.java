6986cf2239946d38d4f371496294f41c57b1def6
BrandstadterViktoria <nunviktoria@gmail.com>
2017-10-18 11:33:15 +0200

    Sample classes which will implement the methods of the interface class

---
+++ InterfacesAndAbstract/InterfacesPractice1/src/Players.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Players implements Saver {
+    @Override
+    public void getValues(ArrayList<String> savedValues) {
+
+    }
+
+    @Override
+    public ArrayList<String> writeTheValues() {
+        return null;
+    }
+}
--- InterfacesAndAbstract/InterfacesPractice1/src/Saver.java
+++ InterfacesAndAbstract/InterfacesPractice1/src/Saver.java
@@ -1,10 +1,6 @@
 import java.util.ArrayList;
 
 public interface Saver{
-
     void getValues (ArrayList<String> savedValues);
     ArrayList<String> writeTheValues();
-
-
-
 }
