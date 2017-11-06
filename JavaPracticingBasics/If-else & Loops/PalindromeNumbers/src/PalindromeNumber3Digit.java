33a927bfbc22263f011399a613be16fec2e6386e
BrandstadterViktoria <nunviktoria@gmail.com>
2017-11-06 11:21:03 +0100

    Create the two loop, in the method palindromeFinder

--- JavaPracticingBasics/If-else & Loops/PalindromeNumbers/src/PalindromeNumber3Digit.java
+++ JavaPracticingBasics/If-else & Loops/PalindromeNumbers/src/PalindromeNumber3Digit.java
@@ -1,4 +1,19 @@
 public class PalindromeNumber3Digit {
 
-    
+    /*The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+    Find the largest palindrome made from the product of two 3-digit */
+
+    public static void main(String[] args) {
+
+    }
+
+    public void palindromeFinder(){
+        for (int i = 100; i <= 500 ; i++) {
+            for (int j = 500; j >= 100 ; j--) {
+                int number = i * j;
+
+            }
+
+        }
+    }
 }
