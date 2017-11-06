8db79a576e08697eefb6fe043b4b941feca75efa
BrandstadterViktoria <nunviktoria@gmail.com>
2017-11-06 11:15:13 +0100

    Various changes

--- JavaPracticingBasics/Strings/ReverseString/src/FizzBuzz.java
+++ JavaPracticingBasics/Strings/ReverseString/src/FizzBuzz.java
@@ -3,28 +3,29 @@ import java.util.List;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-        List<String> result = fizzBuzz(14);
+        List<String> result = new ArrayList<>();
+        result = fizzBuzz(15);
         System.out.println(result);
+
     }
 
     static public List<String> fizzBuzz(int n) {
         List<Integer> nList = new ArrayList<>(n);
         List<String> stringList = new ArrayList<>();
-        for (int i = 1;  i <= n; i++ ) {
+        for (int i = 1; n > i; i++) {
             nList.add(i);
         }
-        for (int i = 0; i < nList.size(); i++) {
-            if (nList.get(i) % 3 == 0 && nList.get(i) % 5 == 0) {
-                stringList.add("FizzBuzz");
-                nList.remove(i);
-            }
+        for (int i = 0; i < nList.size() - 1; i++) {
             if (nList.get(i) % 3 == 0) {
                 stringList.add("Fizz");
-                nList.remove(i);
+
             }
             if (nList.get(i) % 5 == 0) {
                 stringList.add("Buzz");
-                nList.remove(i);
+
+            }
+            if (nList.get(i) % 3 == 0 && nList.get(i) % 5 == 0) {
+                stringList.add("FizzBuzz");
             } else {
                 stringList.add(String.valueOf(nList.get(i)));
             }
