c061a4df4a653e7f038eccbe8d4216570b3bb89b
BrandstadterViktoria <nunviktoria@gmail.com>
2017-11-07 16:05:56 +0100

    Create the first method sumOfSquares, with the solution for the first 10 natural number
    method takes first the sum of the square of the given numbers, then the sum of the numbers and the square of it.

---
+++ AlgorithmPractice/EvenNumberProblems/src/EvenNumberProblems.java
@@ -0,0 +1,35 @@
+public class EvenNumberProblems {
+
+    /*Here are small programs that solve the first 10 even numbered problems from the  Project Euler programming
+    challenge at educational website*/
+
+    public static void main(String[] args) {
+        sumfOfSquares();
+
+    }
+
+    public static void sumfOfSquares() {
+        /* Find the difference between the sum of the squares of the first one hundred natural numbers and the square
+        of the sum*/
+        int sumOfTheSquares = 0;
+        int sumOfNumbersSquares;
+        int sumOfNumbers = 0;
+        for (int i = 1; i <= 10; i++) {
+            int square = i * i;
+            sumOfTheSquares += square;
+        }
+        for (int j = 0; j <= 10; j++) {
+            sumOfNumbers += j;
+        }
+        sumOfNumbersSquares = sumOfNumbers * sumOfNumbers;
+
+        int difference = sumOfNumbersSquares - sumOfTheSquares;
+
+        System.out.println("Sum  of the square of the first 100 natural numbers is: " + sumOfTheSquares + "\n" +
+                "Square of the sum:  " + sumOfNumbersSquares + "\n" +
+                "and the difference is:   " + difference);
+
+    }
+}
+
+
