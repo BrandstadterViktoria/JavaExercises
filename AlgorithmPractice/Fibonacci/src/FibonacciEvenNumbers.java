546714874354e384bbe6601bb0ad23058335fb33
BrandstadterViktoria <nunviktoria@gmail.com>
2017-11-14 10:44:02 +0100

    Delete recursion and use temp variable in order to define fibonacci numbers

--- AlgorithmPractice/Fibonacci/src/FibonacciEvenNumbers.java
+++ AlgorithmPractice/Fibonacci/src/FibonacciEvenNumbers.java
@@ -1,28 +1,24 @@
 public class FibonacciEvenNumbers {
     /*By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.*/
     public static void main(String[] args) {
-
-        int [] fibonacciNumbers = new int [4000000];
+        long number1 = 1;
+        long fibonacci;
         long sum = 0;
-        for (int i = 2; i < 4000000; i++) {
-          fibonacciNumbers[i] = fibonacci(i);
-        }
-        for (int i = 2; i <= fibonacciNumbers.length - 1 ; i++) {
-            if(fibonacciNumbers[i] % 2 == 0){
-                sum += fibonacciNumbers[i];
+        long temp = 0;
+        for (int i = 0; i <= 4000000; i++) {
+            fibonacci = number1 + temp;
+            number1 = fibonacci;
+            temp = fibonacci - temp;
+            System.out.println(fibonacci);
+            if (fibonacci % 2 == 0) {
+                sum += fibonacci;
             }
         }
+
         System.out.println(sum);
+        }
     }
 
 
-    public static int fibonacci(int n) {
-        if (n == 0)
-            return 0;
-        else if (n == 1)
-            return 1;
-        else
-            return fibonacci(n - 1) + fibonacci(n - 2);
-    }
 
-}
+
