d4bfc6884bca95b922d3aa9ddc5a309f79a993cd
BrandstadterViktoria <nunviktoria@gmail.com>
2017-12-11 12:44:49 +0100

    Recreate loops

--- AlgorithmPractice/ProjectEuler/Problems  11 - 30/src/ProjectEulerProblems11_30.java
+++ AlgorithmPractice/ProjectEuler/Problems  11 - 30/src/ProjectEulerProblems11_30.java
@@ -15,17 +15,32 @@ public class ProjectEulerProblems11_30
         /*A Pythagorean triplet is a set of three natural numbers, a < b < c, for which, a2 + b2 = c2
         There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc. */
 
-
-        for (int a = 20, b = 21, c = 22; (a + b + c) < 1001; a++, b++, c++) {
-            int temp = c* c;
-            int temp2 = b *b;
+        for (int a = 20; a <= 10000; a++) {
+            int b = a + 1;
+            int c = b + 1;
+            int product = a * b * c;
+            int temp = c * c;
+            int temp2 = b * b;
             int temp3 = a * a;
-            if(a * a + b * b == c * c && a + b + c == 1000) {
-                    int product = a * b * c;
-                    System.out.println(product);
+            if ((a * a + b * b == c * c) && (a + b + c == 1000)) {
+                System.out.println(product);
+            } else
+                b++;
+            c++;
+            while (!(a * a + b * b == c * c) && c < (1001 - (a + b))) {
+                int tempC = c;
+                while (!(a * a + b * b == c * c) && c < (1001 - (a + b))) {
+                    c++;
+                    if (a + b + c == 1000) {
+                        System.out.println(product);
+                        break;
+                }
+                b++;
+                c = tempC;
                 }
             }
         }
+    }
 
 
     public static void largestProductInAGrid_Problem11() {
