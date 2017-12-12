d9f7341e45989a0fb3874344d7b19ccd6f899c51
BrandstadterViktoria <nunviktoria@gmail.com>
2017-12-12 11:13:49 +0100

    Reorganize the order of existing conditions

--- AlgorithmPractice/ProjectEuler/Problems  11 - 30/src/ProjectEulerProblems11_30.java
+++ AlgorithmPractice/ProjectEuler/Problems  11 - 30/src/ProjectEulerProblems11_30.java
@@ -15,29 +15,27 @@ public class ProjectEulerProblems11_30
         /*A Pythagorean triplet is a set of three natural numbers, a < b < c, for which, a2 + b2 = c2
         There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc. */
 
-        for (int a = 20; a <= 10000; a++) {
+        for (int a = 20; a <= 100; a++) {
             int b = a + 1;
             int c = b + 1;
-            int product = a * b * c;
-            int temp = c * c;
-            int temp2 = b * b;
-            int temp3 = a * a;
+            int product;
             if ((a * a + b * b == c * c) && (a + b + c == 1000)) {
+                product = a * b * c;
                 System.out.println(product);
-            } else
+            } else {
                 b++;
-            c++;
-            while (!(a * a + b * b == c * c) && c < (1001 - (a + b))) {
-                int tempC = c;
-                while (!(a * a + b * b == c * c) && c < (1001 - (a + b))) {
-                    c++;
-                    if (a + b + c == 1000) {
-                        System.out.println(product);
-                        break;
+                c++;
+                while (!(a * a + b * b == c * c) && b < (1001 - (a + c))) {
+                    while (!(a * a + b * b == c * c) && c < (1001 - (a + b))) {
+                        c++;
+                    }
+                    b++;
+                    c = b + 1;
                 }
-                b++;
-                c = tempC;
-                }
+            }
+            if (a + b + c == 1000) {
+                product = a * b * c;
+                System.out.println(product);
             }
         }
     }
