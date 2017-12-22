3722992874eeb0d694f446922899796ca67fcb57
BrandstadterViktoria <nunviktoria@gmail.com>
2017-12-22 11:40:34 +0100

    Fill Employee class with fields and setters-getters and constructor which needed

--- DataStructures/src/linkedList/Employee.java
+++ DataStructures/src/linkedList/Employee.java
@@ -1,4 +1,36 @@
 package linkedList;
 
 public class Employee{
+
+    private String name;
+    private int iDNumber;
+
+    public Employee(String name, int iDNumber) {
+        this.name = name;
+        this.iDNumber = iDNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getiDNumber() {
+        return iDNumber;
+    }
+
+    public void setiDNumber(int iDNumber) {
+        this.iDNumber = iDNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", iDNumber=" + iDNumber +
+                '}';
+    }
 }
