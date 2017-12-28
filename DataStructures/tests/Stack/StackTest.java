d88010479b34fd87590b045f74a227b37ad30c9d
BrandstadterViktoria <nunviktoria@gmail.com>
2017-12-28 11:17:01 +0100

    Preapare testcases with various strings

--- DataStructures/tests/Stack/StackTest.java
+++ DataStructures/tests/Stack/StackTest.java
@@ -4,8 +4,42 @@ import org.junit.Test;
 
 public class StackTest {
     @Test
-    public void isItAPalindrome() throws Exception {
-        
+    public void isItAPalindrome_test1() throws Exception {
+        Stack test1 = new Stack("abccba");
+        for (char character : test1.getWord().toCharArray()) {
+            test1.push(character);
+        }
+    }
+
+    @Test
+    public void isItAPalindrome_test2() throws Exception {
+            Stack test2 = new Stack("Was it a car or a cat I saw?");
+            for (char character : test2.getWord().toCharArray()) {
+                test2.push(character);
+        }
+    }
+
+    @Test
+    public void isItAPalindrome_test3() throws Exception {
+        Stack test3 = new Stack("I did, did I?");
+        for (char character : test3.getWord().toCharArray()) {
+            test3.push(character);
+        }
+
+    }
+    @Test
+    public void isItAPalindrome_test4() throws Exception {
+        Stack test4 = new Stack("hello");
+        for (char character : test4.getWord().toCharArray()) {
+            test4.push(character);
+        }
+    }
+    @Test
+    public void isItAPalindrome_test5() throws Exception {
+        Stack test5 = new Stack("Don't nod");
+        for (char character : test5.getWord().toCharArray()) {
+            test5.push(character);
+        }
     }
 
 }
