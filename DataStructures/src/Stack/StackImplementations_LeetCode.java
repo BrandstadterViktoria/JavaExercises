29c7512c23584345b64d1801d85bd6f28f3a5d38
BrandstadterViktoria <nunviktoria@gmail.com>
2017-12-29 11:49:41 +0100

    Solution with psvm and print

--- DataStructures/src/Stack/StackImplementations_LeetCode.java
+++ DataStructures/src/Stack/StackImplementations_LeetCode.java
@@ -1,28 +1,37 @@
 package Stack;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
 public class StackImplementations_LeetCode {
 
-    public int[] nextGreaterElement(int[]  findNums, int[] nums) {
-        /*Input: nums1 = [4,1,2], nums2 = [1,3,4,2].
+    public static void main(String[] args) {
+        int[]  nums1 = {4,1,2};
+        int [] nums2 = {1,3,4,2};
+        int [] result = nextGreaterElement(nums1, nums2);
+
+        for (int i = 0; i < result.length ; i++) {
+            System.out.println(result[i]);
+
+        }
+
+    }
+
+    public static int[] nextGreaterElement(int[]  nums1, int[] nums2) {
+        /*Input: nums1 = ], nums2 = [1,3,4,2].
         Output: [-1,3,-1]*/
 
      Map<Integer, Integer> map = new HashMap<>();
-     Stack<Integer> stack = new Stack<>();
-     for(int i = nums.length-1; i >= 0; i--){
-         while(!stack.empty() && nums[i] > stack.peek())
-             stack.pop();
-         map.put(nums[i], (stack.empty())? -1 : stack.peek());
-         stack.push(nums[i]);
+     Stack<Integer> stackNums2 = new Stack<>();
+     for(int i = nums2.length-1; i >= 0; i--){
+         while(!stackNums2.empty() && nums2[i] > stackNums2.peek())
+         stackNums2.pop();
+         map.put(nums2[i], (stackNums2.empty())? -1 : stackNums2.peek());
+         stackNums2.push(nums2[i]);
         }
-
-
-
-
-
-        return findNums;
+        for (int i = 0; i < nums1.length ; i++) {
+            nums1[i] = map.get(nums1[i]);
+        }
+        return nums1;
     }
 }
