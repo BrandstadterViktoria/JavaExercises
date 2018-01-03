efab8ae637b250f698ba5404253eb4da48811e86
BrandstadterViktoria <nunviktoria@gmail.com>
2018-01-03 20:49:50 +0100

    Create the class for LeetCode tasks with the first method

---
+++ AlgorithmPractice/LeetCode/src/RandomLeetCodeExercises_1.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class RandomLeetCodeExercises_1 {
+
+    public static void main(String[] args) {
+
+        
+
+    }
+
+    public List<Integer> selfDividingNumbers(int left, int right) {
+
+
+}
+}
