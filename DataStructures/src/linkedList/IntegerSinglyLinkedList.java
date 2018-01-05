8b8b317bcefe09e30d52d510f5af17b44242baeb
BrandstadterViktoria <nunviktoria@gmail.com>
2018-01-05 19:29:30 +0100

    Change in the addToTheListInSortedOrder method

--- DataStructures/src/linkedList/IntegerSinglyLinkedList.java
+++ DataStructures/src/linkedList/IntegerSinglyLinkedList.java
@@ -17,26 +17,27 @@ public class IntegerSinglyLinkedList {
         if (head == null || newNode.getNumber() <= head.getNumber()) {
             newNode.setNext(head);
             head = newNode;
-        }
+            size++;
 
-        IntegerNodeSinglyLinkedList current = head.getNext();
-        IntegerNodeSinglyLinkedList previous = head;
-        while (current != null && current.getNumber() < newNumber) {
-            previous = current;
-            current = current.getNext();
+        } else {
+            IntegerNodeSinglyLinkedList current = head.getNext();
+            IntegerNodeSinglyLinkedList previous = head;
+            while (current != null && current.getNumber() < newNumber) {
+                previous = current;
+                current = current.getNext();
+            }
+            newNode.setNext(current);
+            previous.setNext(newNode);
+            size++;
         }
-        newNode.setNext(current);
-        previous.setNext(newNode);
-
-        size++;
     }
 
     public void print() {
         IntegerNodeSinglyLinkedList current = head;
         System.out.println("HEAD");
-        while (current.getNext() != null) {
+        while (current != null) {
             System.out.println(current);
-            System.out.println("=>");
+            System.out.println("->");
             current = current.getNext();
         }
     }
