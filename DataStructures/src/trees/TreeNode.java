843a18b6ccaa50674e1f77e0dcbdccd92cedfdb6
BrandstadterViktoria <nunviktoria@gmail.com>
2018-01-07 17:11:30 +0100

    Create the get TreeNode method

--- DataStructures/src/trees/TreeNode.java
+++ DataStructures/src/trees/TreeNode.java
@@ -10,29 +10,64 @@ public class TreeNode {
         this.data = data;
     }
 
-    public int getData() {
-        return data;
-    }
 
-    public void setData(int data) {
-        this.data = data;
-    }
-
-    public void insert(int value){
-        if( data == value){
+    public void insert(int value) {
+        if (data == value) {
             return;
         }
-        if( value < data){
-            if(leftChild == null){
+        if (value < data) {
+            if (leftChild == null) {
                 leftChild = new TreeNode(value);
-            }else {
+            } else {
                 leftChild.insert(value);
             }
-            if(rightChild == null){
+            if (rightChild == null) {
                 rightChild = new TreeNode(value);
-            }else{
+            } else {
                 rightChild.insert(value);
             }
         }
     }
-}
+
+    public TreeNode get(int value) {
+        if (data == value) {
+            return this;
+        }
+        if (value < data) {
+            if (leftChild != null) {
+                return leftChild;
+            }
+        } else {
+            if (rightChild != null) {
+                return rightChild;
+            }
+
+        }
+
+        return null;
+    }
+
+
+        public int getData () {
+            return data;
+        }
+        public void setData (int data){
+            this.data = data;
+        }
+
+        public TreeNode getLeftChild () {
+            return leftChild;
+        }
+
+        public void setLeftChild (TreeNode leftChild){
+            this.leftChild = leftChild;
+        }
+
+        public TreeNode getRightChild () {
+            return rightChild;
+        }
+
+        public void setRightChild (TreeNode rightChild){
+            this.rightChild = rightChild;
+        }
+    }
