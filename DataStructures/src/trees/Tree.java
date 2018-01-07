09c5e7dd824c8c886ee955e95fc2f4e325c9e94a
BrandstadterViktoria <nunviktoria@gmail.com>
2018-01-07 17:55:01 +0100

    Create in the Tree class the methods insert, min, max and getters and setters

--- DataStructures/src/trees/Tree.java
+++ DataStructures/src/trees/Tree.java
@@ -1,4 +1,38 @@
 package trees;
 
 public class Tree {
+
+    private TreeNode root;
+
+    public void insert(int value){
+     if(root == null){
+         root = new TreeNode(value);
+     }else{
+         root.insert(value);
+     }
+     }
+
+     public int min(){
+        if(root == null){
+            return Integer.MIN_VALUE;
+        }else{
+            return root.min();
+        }
+     }
+     public int max(){
+        if(root == null){
+            return Integer.MAX_VALUE;
+        }else{
+            return root.max();
+        }
+     }
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(TreeNode root) {
+        this.root = root;
+    }
 }
+
