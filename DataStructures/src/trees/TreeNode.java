a40b01ff67ac6c5a7033a289ba28c23f1e9f1013
BrandstadterViktoria <nunviktoria@gmail.com>
2018-01-07 17:02:42 +0100

    Create in the TreeNode class insert method and values

--- DataStructures/src/trees/TreeNode.java
+++ DataStructures/src/trees/TreeNode.java
@@ -2,4 +2,37 @@ package trees;
 
 public class TreeNode {
 
+    private int data;
+    private TreeNode leftChild;
+    private TreeNode rightChild;
+
+    public TreeNode(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public void insert(int value){
+        if( data == value){
+            return;
+        }
+        if( value < data){
+            if(leftChild == null){
+                leftChild = new TreeNode(value);
+            }else {
+                leftChild.insert(value);
+            }
+            if(rightChild == null){
+                rightChild = new TreeNode(value);
+            }else{
+                rightChild.insert(value);
+            }
+        }
+    }
 }
