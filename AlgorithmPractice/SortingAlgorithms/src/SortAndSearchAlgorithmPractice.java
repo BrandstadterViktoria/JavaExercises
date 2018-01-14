43f6eec2d604d0fe5d0c969121f1753b6d8cadab
BrandstadterViktoria <nunviktoria@gmail.com>
2018-01-14 11:34:57 +0100

    Create insertionsort adaptation

--- AlgorithmPractice/SortingAlgorithms/src/SortAndSearchAlgorithmPractice.java
+++ AlgorithmPractice/SortingAlgorithms/src/SortAndSearchAlgorithmPractice.java
@@ -1,2 +1,30 @@
+import java.util.Arrays;
+
 public class SortAndSearchAlgorithmPractice {
+
+    public static void main(String[] args) {
+
+        int [] sortedArray1 = {23, 35, 78};
+        int [] sortedArray2 = {2, 19, 25, 49, 82};
+    }
+
+    public int [] mergeSortedOrder(int [] sortedArrayB, int [] sortedArrayA){
+
+    /*You are given two sorted arrays, A and B, where A has a large enough buffer at the end to hold B.
+    Write a method to merge B into A in sorted order.*/
+
+    sortedArrayB = Arrays.copyOf(sortedArrayB,sortedArrayA.length +sortedArrayB.length);
+
+        for (int sortedArrayAPosition = 1; sortedArrayAPosition < sortedArrayA.length ; sortedArrayAPosition ++) {
+            int j;
+            int toInsertElement = sortedArrayA[sortedArrayAPosition];
+            for (int j = sortedArrayAPosition; j > 0 && sortedArrayB[j] > toInsertElement; j --){
+                sortedArrayB[j] =  sortedArrayA[j];
+
+            }
+            sortedArrayB[j] = toInsertElement;
+        }
+
+        return sortedArrayB;
+    }
 }
