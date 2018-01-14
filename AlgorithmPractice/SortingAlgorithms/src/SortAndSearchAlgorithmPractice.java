b45a5e0faace1d704efe3fd6664a7eefcb81e8c7
BrandstadterViktoria <nunviktoria@gmail.com>
2018-01-14 13:33:53 +0100

    Clear the body of the method, insertion sort doesnt work for the problem

--- AlgorithmPractice/SortingAlgorithms/src/SortAndSearchAlgorithmPractice.java
+++ AlgorithmPractice/SortingAlgorithms/src/SortAndSearchAlgorithmPractice.java
@@ -4,27 +4,22 @@ public class SortAndSearchAlgorithmPractice {
 
     public static void main(String[] args) {
 
-        int [] sortedArray1 = {23, 35, 78};
-        int [] sortedArray2 = {2, 19, 25, 49, 82};
-    }
+        int[] sortedArray1 = {23, 35, 78};
+        int[] sortedArray2 = {2, 19, 25, 49, 82};
 
-    public int [] mergeSortedOrder(int [] sortedArrayB, int [] sortedArrayA){
+        int [] result = mergeSortedOrder(sortedArray2, sortedArray1);
+        for (int i = 0; i < result.length ; i++) {
+            System.out.println(i);
+        }
+    }
+        public static int[] mergeSortedOrder ( int[] sortedArrayB, int[] sortedArrayA){
 
     /*You are given two sorted arrays, A and B, where A has a large enough buffer at the end to hold B.
     Write a method to merge B into A in sorted order.*/
 
-    sortedArrayB = Arrays.copyOf(sortedArrayB,sortedArrayA.length +sortedArrayB.length);
 
-        for (int sortedArrayAPosition = 1; sortedArrayAPosition < sortedArrayA.length ; sortedArrayAPosition ++) {
-            int j;
-            int toInsertElement = sortedArrayA[sortedArrayAPosition];
-            for (int j = sortedArrayAPosition; j > 0 && sortedArrayB[j] > toInsertElement; j --){
-                sortedArrayB[j] =  sortedArrayA[j];
 
-            }
-            sortedArrayB[j] = toInsertElement;
+           
         }
-
-        return sortedArrayB;
     }
-}
+
