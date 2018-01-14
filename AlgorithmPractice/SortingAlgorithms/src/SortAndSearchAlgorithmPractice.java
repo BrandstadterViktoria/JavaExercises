9c691fbe5cf8dd32f3f82b12b6f78a2e7fdaa00c
BrandstadterViktoria <nunviktoria@gmail.com>
2018-01-14 15:07:07 +0100

    Solution for merge two sorted arrays

--- AlgorithmPractice/SortingAlgorithms/src/SortAndSearchAlgorithmPractice.java
+++ AlgorithmPractice/SortingAlgorithms/src/SortAndSearchAlgorithmPractice.java
@@ -6,14 +6,11 @@ public class SortAndSearchAlgorithmPractice {
 
         int[] sortedArrayA = {23, 35, 78, 0, 0, 0, 0, 0};
         int[] sortedArrayB = {2, 19, 25, 49, 82};
+        mergeSortedOrder(sortedArrayA,sortedArrayB);
 
-        int[] result = mergeSortedOrder(sortedArrayA, sortedArrayB);
-        for (int i = 0; i < result.length; i++) {
-            System.out.println(i);
-        }
-    }
+ }
 
-    public static int[] mergeSortedOrder(int[] sortedArrayA, int[] sortedArrayB) {
+    public static void mergeSortedOrder(int[] sortedArrayA, int[] sortedArrayB) {
 
     /*You are given two sorted arrays, A and B, where A has a large enough buffer at the end to hold B.
     Write a method to merge B into A in sorted order.*/
@@ -23,17 +20,23 @@ public class SortAndSearchAlgorithmPractice {
         int lastIndexOfB = sortedArrayB.length - 1;
 
         while (lastIndexOfB >= 0) {
-            if (sortedArrayA[lastIndexofA] < sortedArrayB[lastIndexOfB]) {
-                sortedArrayA[indexOfMerged] = sortedArrayB[lastIndexOfB];
-                lastIndexOfB--;
-            } else {
+
+            if (lastIndexofA >= 0 && sortedArrayA[lastIndexofA] > sortedArrayB[lastIndexOfB]) {
                 sortedArrayA[indexOfMerged] = sortedArrayA[lastIndexofA];
                 lastIndexofA--;
+            } else {
+                sortedArrayA[indexOfMerged] = sortedArrayB[lastIndexOfB];
+                lastIndexOfB--;
+
             }
             indexOfMerged--;
 
         }
-        return sortedArrayA;
+        for (int i = 0; i < sortedArrayA.length ; i++) {
+            System.out.println(i);
+        }
+
     }
 }
+
 
