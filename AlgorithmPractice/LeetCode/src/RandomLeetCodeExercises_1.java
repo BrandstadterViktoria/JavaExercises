62033e569e973b6b5f937dfeb5bcfb25f1132c2f
BrandstadterViktoria <nunviktoria@gmail.com>
2018-01-27 15:11:19 +0100

    Create the method for finding duplicates in an array with loops, 27 testcase passed from 28

--- AlgorithmPractice/LeetCode/src/RandomLeetCodeExercises_1.java
+++ AlgorithmPractice/LeetCode/src/RandomLeetCodeExercises_1.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -7,9 +8,10 @@ public class RandomLeetCodeExercises_1 {
 
     public static void main(String[] args) {
 
-        int[] numbers = {22, 22, 33, 44, 33, 55, 44, 9, 55};
+        int[] numbers = {1,1};
         int[] numbers1 = {1, 0, 1};
-        System.out.println(singleNumber_LC136(numbers));
+//        System.out.println(singleNumber_LC136(numbers));
+        System.out.println(findDuplicates(numbers));
 
     }
 
@@ -26,6 +28,43 @@ public class RandomLeetCodeExercises_1 {
         }
 
         return -1;
+    }
+
+    public static List<Integer> findDuplicates(int[] nums) {
+        /* Input:[4,3,2,7,8,2,3,1], Output: [2,3]*/
+        List<Integer> duplicates = new ArrayList<>();
+        Arrays.sort(nums);
+        int current;
+        int previous;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums.length == 2) {
+                current = nums[0];
+                previous = nums[1];
+                if (previous == current) {
+                    if (!duplicates.contains(nums[i])) {
+                        duplicates.add(nums[i]);
+                    }
+                }
+            }
+            if (i == 0) {
+                continue;
+            } else {
+                previous = nums[i - 1];
+                current = nums[i];
+                if (previous == current) {
+                    if (!duplicates.contains(nums[i])) {
+                        duplicates.add(nums[i]);
+                    }
+                }
+            }
+            if(i == nums.length -1 && (nums[nums.length - 1] == nums[nums.length - 2])){
+                   if(!duplicates.contains(nums[i])){
+                       duplicates.add(nums[i]);
+                }
+            }
+
+        }
+        return duplicates;
     }
 }
 
