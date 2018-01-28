d1dd0c6bb4a5a98622d938a9d6ed163ff48e557c
BrandstadterViktoria <nunviktoria@gmail.com>
2018-01-28 15:58:36 +0100

    Create the Node innerclass for maxSumOfThreeSubarrays_LC689 method

--- AlgorithmPractice/LeetCode/src/RandomLeetCodeExercises_1.java
+++ AlgorithmPractice/LeetCode/src/RandomLeetCodeExercises_1.java
@@ -56,9 +56,37 @@ public class RandomLeetCodeExercises_1 {
         return duplicates;
     }
 
-    public static  int[] maxSumOfThreeSubarrays_LC689(int[] nums, int k) {
+    public class Node {
+
+        private int data;
+
+        private List<Node> children = new ArrayList<>();
+
+        private Node parent;
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+        public Node addChild(Node child) {
+            child.setParent(this);
+            this.children.add(child);
+            return child;
+        }
+        private void setParent(Node parent) {
+            this.parent = parent;
+        }
+
+        public  int[] maxSumOfThreeSubarrays_LC689(int[] nums, int k) {
         /*Input: [1,2,1,2,6,7,5,1], k= 2
           Output: [0, 3, 5]*/
+
+            for (int i = 0; i < nums.length ; i++) {
+            children.add(new Node(nums[i]));
+                if(i % k == 0){
+
+                }
+            }
 
 
 
