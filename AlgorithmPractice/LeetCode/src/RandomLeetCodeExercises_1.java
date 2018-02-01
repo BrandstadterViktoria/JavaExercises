da821ef26c7f48a0daf9db5fba09123d04894378
BrandstadterViktoria <nunviktoria@gmail.com>
2018-02-01 15:59:14 +0100

    Create method for LeetCode problem771 with solution attempt

--- AlgorithmPractice/LeetCode/src/RandomLeetCodeExercises_1.java
+++ AlgorithmPractice/LeetCode/src/RandomLeetCodeExercises_1.java
@@ -1,18 +1,18 @@
 
 import java.util.*;
-import java.util.stream.Collectors;
+
 
 public class RandomLeetCodeExercises_1 {
 
     public static void main(String[] args) {
 
-        int[] numbers = {4, 3, 2, 7, 8, 2, 3, 1};
+        int[] numbers = {1, 2, 1, 2, 6, 7, 5, 1};
         int[] numbers1 = {1, 0, 1};
 //        System.out.println(singleNumber_LC136(numbers));
 //        System.out.println(findDuplicates_LC442(numbers));
 
-        int [] result = maxSumOfThreeSubarrays_LC689(numbers, 2);
-        for (int i = 0; i < result.length ; i++) {
+        int[] result = maxSumOfThreeSubarrays_LC689(numbers, 2);
+        for (int i = 0; i < result.length; i++) {
             System.out.println(result[i]);
         }
     }
@@ -57,27 +57,45 @@ public class RandomLeetCodeExercises_1 {
         }
         return duplicates;
     }
-        public static int [] maxSumOfThreeSubarrays_LC689(int[] nums, int k) {
+
+    public static int[] maxSumOfThreeSubarrays_LC689(int[] nums, int k) {
         /*Input: [1,2,1,2,6,7,5,1], k= 2
         4, 3, 2, 7, 8, 2, 3, 1
           Output: [0, 3, 5]*/
-            TreeMap<Integer, Integer> sumsAndIndexnumbers = new TreeMap<>();
-            int sumOfSubarrays = 0;
-            for (int i = 0, index = 0; i < nums.length; i++, index++) {
-                sumOfSubarrays += nums[i];
-                if ((i + 1) % k == 0) {
-                    sumsAndIndexnumbers.put(sumOfSubarrays, (i + 1 - k));
-                    if (sumsAndIndexnumbers.size() > 3 && sumOfSubarrays == sumsAndIndexnumbers.firstKey()) {
-                        sumsAndIndexnumbers.remove(sumsAndIndexnumbers.firstKey());
-                    }
-                    sumOfSubarrays = 0;
+        TreeMap<Integer, Integer> sumsAndIndexnumbers = new TreeMap<>();
+        TreeMap<Integer, Integer> duplicates = new TreeMap<>();
+        int sumOfSubarrays = 0;
+        for (int i = 0, index = 0; i < nums.length; i++, index++) {
+            sumOfSubarrays += nums[i];
+            if ((i + 1) % k == 0) {
+                sumsAndIndexnumbers.put(sumOfSubarrays, (i + 1 - k));
+                if (sumsAndIndexnumbers.size() > 3 && sumOfSubarrays == sumsAndIndexnumbers.firstKey()) {
+                    sumsAndIndexnumbers.remove(sumsAndIndexnumbers.firstKey());
                 }
+                sumOfSubarrays = 0;
             }
-            Collection<Integer> values = sumsAndIndexnumbers.values();
-            int[] indexNumbers = values.stream().mapToInt(i->i).toArray();
-            return indexNumbers;
         }
+        int[] indexNumbers = sumsAndIndexnumbers.values().stream().mapToInt(i -> i).toArray();
+        Arrays.sort(indexNumbers);
+        return indexNumbers;
     }
+
+    public int numJewelsInStones(String J, String S) {
+      HashMap <Integer, Character> jewels = new HashMap<>();
+      char [] jewelLetters = J.toCharArray();
+      char [] stoneLetters = S.toCharArray();
+      int counter = 0;
+        for (int i = 0; i < jewelLetters.length; i++) {
+            jewels.put(i,jewelLetters[i]);
+        }
+        for (int i = 0; i < stoneLetters.length; i++) {
+            if(jewels.containsValue(stoneLetters[i])){
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
 
 
 
