8aef9d51e3f06589eec055c76f751c85da559deb
BrandstadterViktoria <nunviktoria@gmail.com>
2018-02-02 13:01:02 +0100

    Create method for ArrayPartitionI Problem 561

--- AlgorithmPractice/LeetCode/src/RandomLeetCodeExercises_1.java
+++ AlgorithmPractice/LeetCode/src/RandomLeetCodeExercises_1.java
@@ -7,7 +7,7 @@ public class RandomLeetCodeExercises_1 {
     public static void main(String[] args) {
 
         int[] numbers = {1, 2, 1, 2, 6, 7, 5, 1};
-        int[] numbers1 = {1, 0, 1};
+        int[] numbers1 = {1, 4, 3, 2};
 //        System.out.println(singleNumber_LC136(numbers));
 //        System.out.println(findDuplicates_LC442(numbers));
 //
@@ -15,7 +15,8 @@ public class RandomLeetCodeExercises_1 {
 //        for (int i = 0; i < result.length; i++) {
 //            System.out.println(result[i]);
 //        }
-        System.out.println(detectCapitalUse("LeetCode"));
+        //       System.out.println(detectCapitalUse("LeetCode"));
+        System.out.println(arrayPairSum(numbers1));
     }
 
     public static int singleNumber_LC136(int[] numbers1) {
@@ -82,24 +83,37 @@ public class RandomLeetCodeExercises_1 {
     }
 
     public int numJewelsInStones(String J, String S) {
-      HashMap <Integer, Character> jewels = new HashMap<>();
-      char [] jewelLetters = J.toCharArray();
-      char [] stoneLetters = S.toCharArray();
-      int counter = 0;
+        HashMap<Integer, Character> jewels = new HashMap<>();
+        char[] jewelLetters = J.toCharArray();
+        char[] stoneLetters = S.toCharArray();
+        int counter = 0;
         for (int i = 0; i < jewelLetters.length; i++) {
-            jewels.put(i,jewelLetters[i]);
+            jewels.put(i, jewelLetters[i]);
         }
         for (int i = 0; i < stoneLetters.length; i++) {
-            if(jewels.containsValue(stoneLetters[i])){
+            if (jewels.containsValue(stoneLetters[i])) {
                 counter++;
             }
         }
         return counter;
     }
-    public static boolean detectCapitalUse(String word){
-        return word.matches("([A-Z][a-z]*\\s*)") || word.matches("[A-Z ]+" )|| word.matches("[a-z]+");
+
+    public static boolean detectCapitalUse(String word) {
+        return word.matches("([A-Z][a-z]*\\s*)") || word.matches("[A-Z ]+") || word.matches("[a-z]+");
+    }
+
+    public static int arrayPairSum(int[] nums) {
+        int sum = 0;
+        int min = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i += 1) {
+            min = Math.min(nums[i], nums[i - 1]);
+            sum += min;
+        }
+        return sum;
     }
 }
+
 
 
 
