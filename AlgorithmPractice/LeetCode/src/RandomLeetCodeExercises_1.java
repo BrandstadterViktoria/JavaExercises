364e166a93294b73711a29d0a11d6bc6e1a61796
BrandstadterViktoria <nunviktoria@gmail.com>
2018-02-02 13:16:25 +0100

    Change the condition in for loop usiong Math min

--- AlgorithmPractice/LeetCode/src/RandomLeetCodeExercises_1.java
+++ AlgorithmPractice/LeetCode/src/RandomLeetCodeExercises_1.java
@@ -103,17 +103,17 @@ public class RandomLeetCodeExercises_1 {
     }
 
     public static int arrayPairSum(int[] nums) {
+        int min;
         int sum = 0;
-        int min = 0;
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i += 1) {
-            min = Math.min(nums[i], nums[i - 1]);
-            sum += min;
-        }
+        for (int i = 0; i < nums.length; i += 1)
+            if ((i + 1) % 2 == 0) {
+                min = Math.min(nums[i], nums[i - 1]);
+                sum += min;
+            }
         return sum;
     }
 }
-
 
 
 
