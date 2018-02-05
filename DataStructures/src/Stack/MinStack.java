b389c124ad751dca1d361a14ae453e96a1d44996
BrandstadterViktoria <nunviktoria@gmail.com>
2018-02-05 10:28:18 +0100

    Change the push method in order to create on sorted order the Stack

--- DataStructures/src/Stack/MinStack.java
+++ DataStructures/src/Stack/MinStack.java
@@ -1,21 +1,30 @@
 package Stack;
 public class MinStack {
     private int [] arrayForStack;
-    private int top = -1;
+    private int top;
 
     public MinStack() {
         arrayForStack = new int [10];
     }
 
     public void push(int x) {
-     if(top > -1 && x < arrayForStack[top]){
-         arrayForStack[top -1] = x;
-         ++ top;
-     }else {
-         arrayForStack[top] = x;
-         ++ top;
-     }
-
+         if(top == 0 || top > 0 && x > arrayForStack[top -1]){
+             arrayForStack[top] = x;
+             ++top;
+         }
+        else if ((top > 0 && x <= arrayForStack[top -1])) {
+             int current;
+             int i;
+            for (i = top -1; i >= 0 ; i--) {
+                if (arrayForStack[i] > x) {
+                   current = arrayForStack[i];
+                   arrayForStack[i +1] = current;
+                    continue;
+                }
+            }
+             arrayForStack[i + 1] = x;
+             ++top;
+        }
     }
 
     public void pop() {
