d24fb8e99775701697deba3f8659f446415f8845
BrandstadterViktoria <nunviktoria@gmail.com>
2018-02-15 09:48:19 +0100

    Redefine constructor

--- AlgorithmPractice/Princeton/src/Percolation.java
+++ AlgorithmPractice/Princeton/src/Percolation.java
@@ -1,33 +1,26 @@
-
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
-import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-
 public class Percolation {
 
-    public boolean isBlocked [] [];
+    public boolean initialStateBlocked [] [];
 
     public Percolation(int n) {
-
-        this.isBlocked  = new boolean[n][n];
-        int rows = (int) (Math.ceil(1.0 / n));
-        int columns = (int) (Math.ceil(1.0 / n));
-
-        QuPoint2Deue<>[][] grid = (Queue<Point2D>[][]) new Queue[rows + 2][columns + 2];
-        for (int i = 0; i <= rows + 1; i++) {
-            for (int j = 0; j <= columns + 1; j++) {
-                grid[i][j] = new Queue<>();
-                isBlocked[i][j] = false;
+        this.initialStateBlocked= new boolean[n][n];
+        for (int i = 0; i <= n + 1; i++) {
+            for (int j = 0; j <= n + 1; j++) {
+                initialStateBlocked[i][j] = false;
             }
         }
     }
     // open site (row, col) if it is not open already
     public void open(int row, int col){
+        if(!isOpen(row, col)){
+
+
+        }
 
     }
     // is site (row, col) open?
     public boolean isOpen(int row, int col){
-        return isBlocked [row][col];
+        return initialStateBlocked[row][col];
 
     }
     // is site (row, col) full?
