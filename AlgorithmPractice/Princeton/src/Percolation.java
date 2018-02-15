2d103f44471f87dbb84a3a0546b3a84b68f0b04a
BrandstadterViktoria <nunviktoria@gmail.com>
2018-02-15 14:06:10 +0100

    Create the nodeInt method which defines the int for the WeightedQuickUnionUF and start to define open method

--- AlgorithmPractice/Princeton/src/Percolation.java
+++ AlgorithmPractice/Princeton/src/Percolation.java
@@ -1,26 +1,46 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+import static edu.princeton.cs.algs4.StdRandom.uniform;
+
 public class Percolation {
 
-    public boolean initialStateBlocked [] [];
+    public boolean theGrid [] [];
+    public int size;
+    public WeightedQuickUnionUF weightedQuickUnionUF;
 
     public Percolation(int n) {
-        this.initialStateBlocked= new boolean[n][n];
+        weightedQuickUnionUF = new WeightedQuickUnionUF(n *n);
+        this.size = n;
+        this.theGrid  = new boolean[n][n];
         for (int i = 0; i <= n + 1; i++) {
             for (int j = 0; j <= n + 1; j++) {
-                initialStateBlocked[i][j] = false;
+                theGrid [i][j] = false;
             }
         }
+
     }
+    public int nodeInt(int row, int col){
+       return row * size - size + col;
+    }
+
     // open site (row, col) if it is not open already
     public void open(int row, int col){
-        if(!isOpen(row, col)){
+         row = uniform(size);
+         col = uniform(size);
+        if(isOpen(row, col)){
+            if(isOpen(row -1,col){
+                weightedQuickUnionUF.union( [row][col],[row -1][col]);
+
+            }
+
 
 
         }
 
     }
+
     // is site (row, col) open?
     public boolean isOpen(int row, int col){
-        return initialStateBlocked[row][col];
+        return theGrid [row][col];
 
     }
     // is site (row, col) full?
