e4edda103bc0d21fa5d1ebdda6a0ce2b3234fe8f
BrandstadterViktoria <nunviktoria@gmail.com>
2018-02-28 11:40:51 +0100

    Create classes

---
+++ AlgorithmPractice/Princeton/DequesAndRandomizedQueues/src/Deque.java
@@ -0,0 +1,15 @@
+
+public class Deque {
+
+    private class Node {
+        String item;
+        Node next;
+    }
+
+
+    public Deque(){
+
+    }
+
+
+}
---
+++ AlgorithmPractice/Princeton/DequesAndRandomizedQueues/src/Permutation.java
@@ -0,0 +1,12 @@
+
+public class Permutation {
+    public static void main(String[] args) {
+        int returnStrings = 0;
+        returnStrings = Integer.parseInt(args[0]);
+
+
+
+
+
+    }
+}
