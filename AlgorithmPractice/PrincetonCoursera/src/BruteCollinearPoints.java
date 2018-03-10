3f300919c64833830b523d709132f80efb48f5e1
BrandstadterViktoria <nunviktoria@gmail.com>
2018-03-10 10:43:36 +0100

    Specify the case if points are collinear and create test client class

--- AlgorithmPractice/PrincetonCoursera/src/BruteCollinearPoints.java
+++ AlgorithmPractice/PrincetonCoursera/src/BruteCollinearPoints.java
@@ -12,38 +12,41 @@ public class BruteCollinearPoints {
             if (points[i] == null) {
                 throw new IllegalArgumentException("Illegal point");
             }
-                for (int j = 0; j < points.length; j++) {
-                    if (points[i].compareTo(points[j]) == 0) {
-                        throw new IllegalArgumentException("Illegal points");
-                    }
+            for (int j = 0; j < points.length; j++) {
+                if (points[i].compareTo(points[j]) == 0) {
+                    throw new IllegalArgumentException("Illegal points");
                 }
-                points = new Point[4];
-                this.points = points;
             }
+            points = new Point[4];
+            this.points = points;
         }
 
-        public int numberOfSegments () {
-            return size;
-        }
+    }
 
-        public LineSegment[] segments () {
-        LineSegment [] lineSegments = new LineSegment[3];
-        int indexOfLineSegments = - 1;
+    public int numberOfSegments() {
+        return size;
+    }
+
+    public LineSegment[] segments() {
+        LineSegment[] lineSegments = new LineSegment[3];
+        if (points[0].slopeTo(points[1]) == points[1].slopeTo(points[2]) && points[0].slopeTo(points[1]) == points[0].slopeTo(points[3])) {
+            lineSegments[0] = new LineSegment(points[0], points[3]);
+        } else {
+            int indexOfLineSegments = -1;
             for (int i = 0; i < points.length; i++) {
                 for (int j = i + 1; j < points.length + 1; j++) {
-                    lineSegments[++indexOfLineSegments] = new LineSegment(points[i],points[j > 3 ? 0 : j]);
+                    lineSegments[++indexOfLineSegments] = new LineSegment(points[i], points[j > 3 ? 0 : j]);
                     for (int k = i + 2; k < points.length + 2; k++) {
-                        lineSegments[++indexOfLineSegments] = new LineSegment(points[j], points[k > 3 ? ( k - 4) : k]);
+                        lineSegments[++indexOfLineSegments] = new LineSegment(points[j], points[k > 3 ? (k - 4) : k]);
                         for (int l = i + 3; l < points.length + 3; l++) {
-                            lineSegments[++indexOfLineSegments] = new LineSegment(points[k],points[l > 3 ? (l - 4) : l]);
+                            lineSegments[++indexOfLineSegments] = new LineSegment(points[k], points[l > 3 ? (l - 4) : l]);
                         }
                     }
 
                 }
 
             }
-
-
         }
-
+        return lineSegments;
     }
+}
