e7cfc44e8bda6bb06f683274c27da7da7c36c47a
BrandstadterViktoria <nunviktoria@gmail.com>
2018-03-10 10:29:28 +0100

    Specify the index number of linesegments

--- AlgorithmPractice/PrincetonCoursera/src/BruteCollinearPoints.java
+++ AlgorithmPractice/PrincetonCoursera/src/BruteCollinearPoints.java
@@ -28,13 +28,14 @@ public class BruteCollinearPoints {
 
         public LineSegment[] segments () {
         LineSegment [] lineSegments = new LineSegment[3];
+        int indexOfLineSegments = - 1;
             for (int i = 0; i < points.length; i++) {
                 for (int j = i + 1; j < points.length + 1; j++) {
-                    lineSegments[0] = new LineSegment(points[i],points[j > 3 ? 0 : j]);
+                    lineSegments[++indexOfLineSegments] = new LineSegment(points[i],points[j > 3 ? 0 : j]);
                     for (int k = i + 2; k < points.length + 2; k++) {
-                        lineSegments[1] = new LineSegment(points[j], points[k > 3 ? ( k - 4) : k]);
+                        lineSegments[++indexOfLineSegments] = new LineSegment(points[j], points[k > 3 ? ( k - 4) : k]);
                         for (int l = i + 3; l < points.length + 3; l++) {
-                            lineSegments[2] = new LineSegment(points[k],points[l > 3 ? (l - 4) : l]);
+                            lineSegments[++indexOfLineSegments] = new LineSegment(points[k],points[l > 3 ? (l - 4) : l]);
                         }
                     }
 
