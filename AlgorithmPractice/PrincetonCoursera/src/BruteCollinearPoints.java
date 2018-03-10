cb216e57ea137a283c65eca7287712d4cdac12a1
BrandstadterViktoria <nunviktoria@gmail.com>
2018-03-10 09:57:11 +0100

    Create constructor and loop in LineSegment method in BruteCollinear class

--- AlgorithmPractice/PrincetonCoursera/src/BruteCollinearPoints.java
+++ AlgorithmPractice/PrincetonCoursera/src/BruteCollinearPoints.java
@@ -1,19 +1,48 @@
 public class BruteCollinearPoints {
 
+    Point[] points;
+    int size = 0;
+
     // finds all line segments containing 4 points
     public BruteCollinearPoints(Point[] points) {
+        if (points == null) {
+            throw new IllegalArgumentException("Illegal points");
+        }
+        for (int i = 0; i < points.length; i++) {
+            if (points[i] == null) {
+                throw new IllegalArgumentException("Illegal point");
+            }
+                for (int j = 0; j < points.length; j++) {
+                    if (points[i].compareTo(points[j]) == 0) {
+                        throw new IllegalArgumentException("Illegal points");
+                    }
+                }
+                points = new Point[4];
+                this.points = points;
+            }
+        }
+
+        public int numberOfSegments () {
+            return size;
+        }
+
+        public LineSegment[] segments () {
+        LineSegment [] lineSegments = new LineSegment[3];
+            for (int i = 0; i < points.length ; i++) {
+                for (int j = 1; j < points.length -1; j++) {
+                    lineSegments[2] = new LineSegment(points[i],points[j]);
+                    for (int k = 0; k < points.length - 2 ; k++) {
+                        lineSegments[1] = new LineSegment(points[j], points[k]);
+                        for (int l = 0; l < points.length - 3 ; l++) {
+                            lineSegments[0] = new LineSegment(points[k],points[l]);
+                        }
+                    }
+
+                }
+
+            }
+
+
+        }
+
     }
-
-    // the number of line segments
-    public           int numberOfSegments() {
-    }
-
-    // the line segments
-    public LineSegment[] segments() {
-        
-    }
-
-}
-
-
-}
