ee420f48b13eab72ec5fe0df6604e91b9c61ff34
BrandstadterViktoria <nunviktoria@gmail.com>
2018-03-25 12:26:48 +0200

    Create Solver class

---
+++ AlgorithmPractice/PrincetonCoursera/src/Solver.java
@@ -0,0 +1,32 @@
+public class Solver {
+
+    public class Node{
+
+
+    }
+
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial) {
+
+
+    }
+    // is the initial board solvable?
+    public boolean isSolvable() {
+
+    }
+
+    // min number of moves to solve initial board; -1 if unsolvable
+    public int moves() {
+
+    }
+
+    // sequence of boards in a shortest solution; null if unsolvable
+    public Iterable<Board> solution() {
+
+    }
+
+    // solve a slider puzzle (given below)
+    public static void main(String[] args){
+
+}
+}
