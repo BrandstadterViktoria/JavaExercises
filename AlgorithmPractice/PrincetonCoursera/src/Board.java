1a3014d00334dfc572df4767b7731e9b5805371c
BrandstadterViktoria <nunviktoria@gmail.com>
2018-04-05 19:26:56 +0200

    Redefine the Board class without using one dimensional array

--- AlgorithmPractice/PrincetonCoursera/src/Board.java
+++ AlgorithmPractice/PrincetonCoursera/src/Board.java
@@ -1,63 +1,57 @@
 import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdRandom;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Board {
 
-    private final int[] blockArray;
     private final int dimension;
     private final int[][] blocks;
     private int manhattan = 0;
     private int blankPositionRow = 0;
     private int blankPositionCol = 0;
+    private Board twin;
     private boolean done = false;
-    private int [][] twinArray;
+    private int hamming = 0;
 
 
     public Board(int[][] blocks) {
         this.blocks = blocks;
         this.dimension = blocks.length;
-        this.twinArray = new int[dimension][dimension];
-        List<Integer> list = new ArrayList<>();
-        List<Integer> goalCoordinates = new ArrayList<>();
+        int goalRow = 0;
+        int goalColumn = 0;
+        int currentElementOfBlockArray = 1;
+        while (currentElementOfBlockArray <= dimension * dimension - 1) {
+            for (int i = 0; i < blocks.length; i++) {
+                for (int j = 0; j < blocks[i].length; j++) {
+                    if (blocks[i][j] == currentElementOfBlockArray) {
+                        int currentManhattan = Math.abs(i - goalRow) + Math.abs(j - goalColumn);
+                        if (currentManhattan > 0) {
+                            hamming++;
+                        }
+                        this.manhattan += currentManhattan;
+                        currentElementOfBlockArray++;
+                        goalRow = (currentElementOfBlockArray - 1) / dimension;
+                        goalColumn = (currentElementOfBlockArray - 1) - (goalRow * dimension);
+                        i = -1;
+                        break;
+                    }
+                }
+
+            }
+        }
         for (int i = 0; i < blocks.length; i++) {
             for (int j = 0; j < blocks[i].length; j++) {
-                list.add(blocks[i][j]);
-                goalCoordinates.add(i);
-                goalCoordinates.add(j);
                 if (blocks[i][j] == 0) {
-                    this.blankPositionRow = i;
-                    this.blankPositionCol = j;
+                    blankPositionRow = i;
+                    blankPositionCol = j;
                 }
             }
         }
-        this.blockArray = new int[list.size()];
-        for (int i = 0; i < blockArray.length; i++) {
-            blockArray[i] = list.get(i);
-        }
-
-        int row = -1;
-        int col;
-        int currentElementOfBlockArray = 1;
-        int goalCoordinate = 0;
-        for (int i = 1; currentElementOfBlockArray <= dimension * dimension - 1; i++) {
-            if (blockArray[i - 1] == currentElementOfBlockArray) {
-                currentElementOfBlockArray++;
-                for (int k = 0; k <= i - 1; k += dimension) {
-                    row++;
-                }
-                col = (i - 1) - (row * dimension);
-                int currentManhattan = Math.abs(row - goalCoordinates.get(goalCoordinate)) + Math.abs(col - goalCoordinates.get(goalCoordinate + 1));
-                goalCoordinate += 2;
-                this.manhattan += currentManhattan;
-                i = 0;
-                row = -1;
-            }
-        }
-
     }
+
 
     public int dimension() {
         return dimension;
@@ -65,12 +59,6 @@ public class Board {
     }
 
     public int hamming() {
-        int hamming = 0;
-        for (int i = 1; i <= dimension * dimension - 1; i++) {
-            if (blockArray[i - 1] != i) {
-                hamming++;
-            }
-        }
         return hamming;
     }
 
@@ -81,56 +69,67 @@ public class Board {
 
 
     public boolean isGoal() {
-        boolean isGoal = false;
-        for (int i = 1; i < blockArray.length; i++) {
-            if (i == blockArray[i - 1]) {
-                isGoal = true;
-                continue;
-            } else {
-                isGoal = false;
-                break;
-            }
-        }
-        return isGoal;
+        return manhattan == 0;
+
+//        boolean isGoal = false;
+//        int [] blockArray = new int [dimension * dimension];
+//        for (int i = 0; i < blocks.length; i++) {
+//            for (int j = 0; j < blocks[i].length; j++) {
+//                blockArray[j + (i * dimension)] = blocks[i][j];
+//            }
+//        }
+//        for (int i = 1; i < blockArray.length; i++) {
+//            if (i == blockArray[i - 1]) {
+//                isGoal = true;
+//                continue;
+//            } else {
+//                isGoal = false;
+//                break;
+//            }
+//        }
+//        return isGoal;
     }
 
 
     public Board twin() {
-       while (!done) {
-           int toSwap1 = StdRandom.uniform(1, dimension * dimension);
-           int toSwap2 = StdRandom.uniform(1, dimension * dimension);
-           while (toSwap1 == toSwap2) {
-               toSwap2 = StdRandom.uniform(1, dimension * dimension);
-           }
-           int[] arrayOfTwinBoard = swap(toSwap1, toSwap2);
-           this.twinArray = to2DArrayConverter(arrayOfTwinBoard);
-           done = true;
-           break;
-       }
-        return new Board(twinArray);
+        while (!done) {
+            int toSwap1 = StdRandom.uniform(0, dimension);
+            int toSwap2 = StdRandom.uniform(0, dimension);
+            int toSwap3 = StdRandom.uniform(0, dimension);
+            int toSwap4 = StdRandom.uniform(0, dimension);
+            while (toSwap1 == toSwap3 && toSwap2 == toSwap4) {
+                toSwap3 = StdRandom.uniform(0, dimension);
+                toSwap4 = StdRandom.uniform(0, dimension);
+            }
+            int[][] arrayOfTwinBoard = swap(toSwap1, toSwap2, toSwap3, toSwap4);
+            twin = new Board(arrayOfTwinBoard);
+            done = true;
+        }
+
+        return twin;
     }
 
     public boolean equals(Object y) {
         if (y == this) return true;
         if (y == null) return false;
         if (y.getClass() != this.getClass()) return false;
-        return (Arrays.equals(((Board) y).getBlockArray(), this.getBlockArray()));
+        Board compare = (Board) y;
+        return Arrays.deepEquals(compare.blocks, this.blocks);
     }
 
     public Iterable<Board> neighbors() {
-        int blankIndex = blankPositionCol + blankPositionRow * dimension;
         Stack<Board> neighbors = new Stack<>();
         if (blankPositionRow != dimension - 1 && blankPositionRow >= 0) {
-            neighbors.push(new Board(to2DArrayConverter(swap(blankIndex, blankIndex + dimension))));
+            neighbors.push(new Board(swap(blankPositionRow,blankPositionCol, blankPositionRow + 1,blankPositionCol)));
         }
         if (blankPositionRow != 0 && blankPositionRow <= dimension - 1) {
-            neighbors.push(new Board(to2DArrayConverter(swap(blankIndex, blankIndex - dimension))));
+            neighbors.push(new Board((swap(blankPositionRow, blankPositionCol, blankPositionRow - 1, blankPositionCol))));
         }
         if (blankPositionCol != dimension - 1 && blankPositionCol >= 0) {
-            neighbors.push(new Board(to2DArrayConverter(swap(blankIndex, blankIndex + 1))));
+            neighbors.push(new Board(swap(blankPositionRow,blankPositionCol, blankPositionRow,blankPositionCol + 1)));
         }
         if (blankPositionCol != 0 && blankPositionCol <= dimension - 1) {
-            neighbors.push(new Board(to2DArrayConverter(swap(blankIndex, blankIndex - 1))));
+            neighbors.push(new Board(swap(blankPositionRow,blankPositionCol, blankPositionRow,blankPositionCol - 1)));
         }
 
         return neighbors;
@@ -149,41 +148,23 @@ public class Board {
 
     }
 
-    private int[] getBlockArray() {
-        return blockArray;
-    }
-
-    private int[] swap(int toSwap, int goalIndexForSwap) {
-        int[] swapped = this.blockArray.clone();
-        int temp = swapped[goalIndexForSwap];
-        swapped[goalIndexForSwap] = swapped[toSwap];
-        swapped[toSwap] = temp;
+    private int[][] swap(int toSwapRow, int toSwapColumn, int goalIndexForSwapRoow, int goalIndexForSwapCol) {
+     int swapped [][] = new int [dimension][dimension];
+        for (int i = 0; i < blocks.length; i++) {
+            for (int j = 0; j < blocks[i].length; j++) {
+                swapped[i][j] = blocks[i][j];
+            }
+        }
+        int temp = swapped[goalIndexForSwapRoow][goalIndexForSwapCol];
+        swapped[goalIndexForSwapRoow][goalIndexForSwapCol] = swapped[toSwapRow][toSwapColumn];
+        swapped[toSwapRow][toSwapColumn] = temp;
         return swapped;
     }
 
 
-    private int[][] to2DArrayConverter(int[] arrayToConvert) {
-        int[][] new2DArray = new int[dimension()][dimension()];
-        for (int row = 0; row < dimension(); row++) {
-            for (int col = 0; col < dimension(); col++) {
-                new2DArray[row][col] = arrayToConvert[col + row * dimension()];
-            }
-        }
-        return new2DArray;
-    }
-
     public static void main(String[] args) {
         // empty method intentionally
 
-        int[][] table = {{2, 8, 3, 4}, {1, 10, 6, 0}, {5, 9, 7, 12}, {13, 14, 11, 15}};
-
-        Board tester = new Board(table);
-        System.out.println(tester.twin());
-        System.out.println(tester.twin());
-        System.out.println(tester.twin());
-        System.out.println(tester.twin());
-        System.out.println(tester.twin());
-        System.out.println(tester.twin());
 
 
     }
