65c622479894243f7423b6ed63c8d9acc5054642
BrandstadterViktoria <nunviktoria@gmail.com>
2018-04-07 13:01:17 +0200

    Redefine insert method

--- AlgorithmPractice/PrincetonCoursera/src/KdTree.java
+++ AlgorithmPractice/PrincetonCoursera/src/KdTree.java
@@ -1,65 +1,83 @@
-import Week3_CollinearPoints.Point;
 import edu.princeton.cs.algs4.Point2D;
-import edu.princeton.cs.algs4.RectHV;
 
 public class KdTree {
 
     private int size;
     Node root;
+    private int level;
 
     private class Node {
-        double x;
-        double y;
+        Point2D p;
         Node left;
         Node right;
-        int level;
 
-        public Node(double x, double y, int level) {
-            this.x = x;
-            this.y = y;
-            this.level = level;
+
+        public Node(Point2D p) {
+            this.p = p;
         }
 
-        public void insertNode(double xC, double yC) {
-            if (xC == x || yC == y) {
+        public void insertNode(Point2D toInsert) {
+            if (toInsert == p) {
                 return;
             }
-            if (xC < x) {
-                if (left == null) {
-                    left = new Node(xC,yC, level);
+            if (level % 2 == 0) {
+                if (toInsert.y() < p.y()) {
+                    if (left == null) {
+                        left = new Node(toInsert);
+                    } else {
+                        level++;
+                        left.insertNode(toInsert);
+
+                    }
                 } else {
-                    left.insertNode(xC,yC);
-                    level = root.level++;
+                    if (right == null) {
+                        right = new Node(toInsert);
+                    } else {
+                        level++;
+                        right.insertNode(toInsert);
+                    }
                 }
             } else {
-                if (right == null) {
-                    right = new Node(xC,yC,level);
+                if (toInsert.x() < p.x()) {
+                    if (left == null) {
+                        left = new Node(toInsert);
+                    } else {
+                        level++;
+                        left.insertNode(toInsert);
+
+                    }
                 } else {
-                    right.insertNode(xC,yC);
-                    level = root.level++;
+                    if (right == null) {
+                        right = new Node(toInsert);
+                    } else {
+                        level++;
+                        right.insertNode(toInsert);
+                    }
                 }
             }
         }
     }
 
+
     // is the set empty?
-    public  boolean isEmpty() {
+    public boolean isEmpty() {
         return size == 0;
     }
 
     // number of points in the set
-    public  int size() {
+    public int size() {
         return size;
     }
 
     // add the point to the set (if it is not already in the set)
-    public  void insert(Point2D p) {
+    public void insert(Point2D p) {
         if (root == null) {
-            root = new Node(p.x(), p.y(),1);
+            root = new Node(p);
+            level = 1;
             size++;
-        }
-        else {
-            root.insertNode(p.x(), p.y());
+        } else {
+            root.insertNode(p);
+            level = 1;
             size++;
         }
     }
@@ -86,14 +104,13 @@ public class KdTree {
 
     }*/
 
-
     // unit testing of the methods (optional)
     public static void main(String[] args) {
-        Point2D w = new Point2D(0.7,0.2);
-        Point2D e = new Point2D(0.5,0.4);
-        Point2D r = new Point2D(0.2,0.3);
-        Point2D t = new Point2D(0.4,0.7);
-        Point2D z = new Point2D(0.9,0.6);
+        Point2D w = new Point2D(0.7, 0.2);
+        Point2D e = new Point2D(0.5, 0.4);
+        Point2D r = new Point2D(0.2, 0.3);
+        Point2D t = new Point2D(0.4, 0.7);
+        Point2D z = new Point2D(0.9, 0.6);
         KdTree tester = new KdTree();
         tester.insert(w);
         tester.insert(e);
@@ -101,5 +118,5 @@ public class KdTree {
         tester.insert(t);
         tester.insert(z);
 
-}
+    }
 }
