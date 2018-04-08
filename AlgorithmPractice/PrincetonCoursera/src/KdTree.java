edb4c27816f67d07a67db5964e06e8a32c498a8e
BrandstadterViktoria <nunviktoria@gmail.com>
2018-04-08 17:02:48 +0200

    Create rectangles

--- AlgorithmPractice/PrincetonCoursera/src/KdTree.java
+++ AlgorithmPractice/PrincetonCoursera/src/KdTree.java
@@ -1,15 +1,19 @@
 import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.StdDraw;
 
 public class KdTree {
 
     private int size;
-    Node root;
+    private Node root;
     private int level;
+    private PointSET points = new PointSET();
 
     private class Node {
-        Point2D p;
-        Node left;
-        Node right;
+        private RectHV rectHV;
+        private Point2D p;
+        private Node left;
+        private Node right;
 
 
         public Node(Point2D p) {
@@ -24,31 +28,36 @@ public class KdTree {
                 if (toInsert.y() < p.y()) {
                     if (left == null) {
                         left = new Node(toInsert);
+                        left.rectHV = new RectHV(rectHV.xmin(),rectHV.ymin(),rectHV.xmax(),p.y());
                     } else {
                         level++;
                         left.insertNode(toInsert);
-
                     }
                 } else {
                     if (right == null) {
                         right = new Node(toInsert);
+                        right.rectHV = new RectHV(rectHV.xmin(),p.y(),rectHV.xmax(),rectHV.ymax());
                     } else {
                         level++;
                         right.insertNode(toInsert);
+
                     }
                 }
             } else {
                 if (toInsert.x() < p.x()) {
                     if (left == null) {
                         left = new Node(toInsert);
+                        left.rectHV = new RectHV(rectHV.xmin(),rectHV.ymin(),p.x(),rectHV.ymax());
+
                     } else {
                         level++;
                         left.insertNode(toInsert);
-
                     }
                 } else {
                     if (right == null) {
                         right = new Node(toInsert);
+                        right.rectHV = new RectHV(p.x(),rectHV.ymin(),rectHV.xmax(),rectHV.ymax());
+
                     } else {
                         level++;
                         right.insertNode(toInsert);
@@ -59,21 +68,20 @@ public class KdTree {
 
         private Node traverse(Node node, Point2D contained) {
             Node result = null;
-            if(node != null){
+            if (node != null) {
                 result = traverse(node.left, contained);
-                if( result != null)
+                if (result != null)
                     return result;
 
-                if(node.p.compareTo(contained) == 0){
+                if (node.p.compareTo(contained) == 0) {
                     System.out.println(contained + " was found at " + node);
                     return node;
                 }
                 result = traverse(node.right, contained);
             }
             return result;
-            }
         }
-
+    }
 
     // is the set empty?
     public boolean isEmpty() {
@@ -89,10 +97,13 @@ public class KdTree {
     public void insert(Point2D p) {
         if (root == null) {
             root = new Node(p);
+            points.insert(p);
+            root.rectHV = new RectHV(0.0,0.0,1.0,1.0);
             level = 1;
             size++;
         } else {
             root.insertNode(p);
+            points.insert(p);
             level = 1;
             size++;
         }
@@ -104,22 +115,27 @@ public class KdTree {
         if (root != null) {
             if (root.p.compareTo(p) == 0) {
                 doesContain = true;
-            } else if (root.traverse(root, p)== null) {
+            } else if (root.traverse(root, p) == null) {
                 doesContain = false;
-            } else if (root.traverse(root,p) != null) {
+            } else if (root.traverse(root, p) != null) {
                 doesContain = true;
             }
         }
         return doesContain;
     }
+    // draw all points to standard draw
+    //Use StdDraw.setPenColor(StdDraw.BLACK) and StdDraw.setPenRadius(0.01) before before drawing the points;
+    // use StdDraw.setPenColor(StdDraw.RED) or StdDraw.setPenColor(StdDraw.BLUE) and StdDraw.setPenRadius()
+    // before drawing the splitting lines.
+    /*public void draw () {
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(0.01);
+        points.draw();
+        StdDraw.line();
 
 
-           /* // draw all points to standard draw
-            public void draw () {
-            }
-
-
-            // all points that are inside the rectangle (or on the boundary)
+    }*/
+           /* // all points that are inside the rectangle (or on the boundary)
             public Iterable<Point2D> range (RectHV rect){
 
 
