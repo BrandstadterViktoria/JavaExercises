8f9685a216c1d5f706f33f89592bef0140bb5295
BrandstadterViktoria <nunviktoria@gmail.com>
2018-06-05 11:02:32 +0200

    Modify the method ancestor in order to break the inner loop, when needed, use with bfs directed path, and change the input for testing

--- AlgorithmPractice/PrincetonCoursera/src/SAP.java
+++ AlgorithmPractice/PrincetonCoursera/src/SAP.java
@@ -2,53 +2,53 @@ import edu.princeton.cs.algs4.*;
 
 public class SAP {
 
-    private Graph G;
+    private Digraph G;
     private final int rootVertex = 0;
 
     // constructor takes a digraph (not necessarily a DAG)
-    public SAP(Graph G) {
+    public SAP(Digraph G) {
         this.G = G;
     }
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
         if (v < 0 || v > G.V() - 1 || w < 0 || w > G.V() - 1) {
-            throw new IllegalArgumentException("The given vertex is not between 0 and " + (G.V()-1));
+            throw new IllegalArgumentException("The given vertex is not between 0 and " + (G.V() - 1));
         }
+        int ancestor = ancestor(v, w);
         int length = 0;
-        BreadthFirstPaths bfsV = new BreadthFirstPaths(G,v);
-        BreadthFirstPaths bfsW = new BreadthFirstPaths(G,w);
-        while (bfsV.pathTo(rootVertex).iterator().hasNext()) {
-              length++;
+        BreadthFirstDirectedPaths bfsV = new BreadthFirstDirectedPaths(G, v);
+        BreadthFirstDirectedPaths bfsW = new BreadthFirstDirectedPaths(G, w);
+        for (int t : bfsV.pathTo(ancestor)) {
+            length++;
         }
-        while (bfsW.pathTo(rootVertex).iterator().hasNext()) {
-            length ++;
+        for (int t : bfsW.pathTo(ancestor)) {
+            length++;
+        }
+        return length - 2;
         }
 
-        return length;
-    }
+        // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
 
-    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
     public int ancestor(int v, int w) {
         if (v < 0 || v > G.V() - 1 || w < 0 || w > G.V() - 1) {
-            throw new IllegalArgumentException("The given vertex is not between 0 and " + (G.V()-1));
+            throw new IllegalArgumentException("The given vertex is not between 0 and " + (G.V() - 1));
         }
-        int ancestor = 0;
-        BreadthFirstPaths bfsV = new BreadthFirstPaths(G,v);
-        BreadthFirstPaths bfsW = new BreadthFirstPaths(G,w);
-        for ( int vs : bfsV.pathTo(rootVertex)) {
-            for ( int vs2 : bfsW.pathTo(rootVertex)) {
-                if (vs == vs2){
+        int ancestor;
+        BreadthFirstDirectedPaths bfsV = new BreadthFirstDirectedPaths(G, v);
+        BreadthFirstDirectedPaths bfsW = new BreadthFirstDirectedPaths(G, w);
+        for (int vs : bfsV.pathTo(rootVertex)) {
+            for (int vs2 : bfsW.pathTo(rootVertex)) {
+                if (vs == vs2) {
                     ancestor = vs;
-                    break;
+                    return ancestor;
                 }
             }
-         }
-
-         return ancestor > 0 ? ancestor : -1;
+        }
+        return -1;
     }
 
-    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+    /*// length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
         if (v == null || w == null) {
             throw new IllegalArgumentException("The iterable has 0 values. ");
@@ -71,19 +71,22 @@ public class SAP {
             throw new IllegalArgumentException("The iterable contains null valued vertex.");
         }
 
-    }
+
+
+    }*/
 
 
     // do unit testing of this class
     public static void main(String[] args) {
         In in = new In(args[0]);
         Digraph G = new Digraph(in);
-        SAP sap = new SAP(G, rootVertex);
-            int v = 3;
-            int w = 11;
+        SAP sap = new SAP(G);
+        while (!StdIn.isEmpty()) {
+            int v = StdIn.readInt();
+            int w = StdIn.readInt();
+            int length = sap.length(v, w);
             int ancestor = sap.ancestor(v, w);
-            int length   = sap.length(v, w);
             StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
-
+        }
     }
 }
