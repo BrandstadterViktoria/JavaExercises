82511c3a590f12651d628e55bc08654c1ee158e0
BrandstadterViktoria <nunviktoria@gmail.com>
2018-06-20 14:47:23 +0200

    Create yieldingX2 and yieldingY2 methods in order to compute the square of the sum of differences the values of red, green and blue components of the neighbouring pixels of the given one

--- AlgorithmPractice/PrincetonCoursera/src/SeamCarver.java
+++ AlgorithmPractice/PrincetonCoursera/src/SeamCarver.java
@@ -1,11 +1,12 @@
 import edu.princeton.cs.algs4.Picture;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.awt.*;
+
 
 public class SeamCarver {
-
     private Picture pictureCopy;
-    private int width = width();
-    private int height = height();
-    private int[][] pixels;
+    //private int[][] pixelsRGB;
 
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
@@ -13,11 +14,11 @@ public class SeamCarver {
             throw new IllegalArgumentException("Arg is illegal");
         }
         this.pictureCopy = new Picture(picture);
-        pixels = new int[width][height];
+     /*   pixelsRGB = new int[width][height];
         for (int i = 0; i < width; i++)
             for (int j = 0; j < height; j++)
-                pixels[i][j] = picture.getRGB(i, j);
-
+                pixelsRGB[i][j] = picture.getRGB(i, j);
+*/
     }
 
     // current picture
@@ -39,14 +40,15 @@ public class SeamCarver {
 
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
-        if (x < 0 || x >= height)
-            throw new IllegalArgumentException("column index must be between 0 and " + (height - 1) + ": " + x);
-        if (y < 0 || y >= width)
-            throw new IllegalArgumentException("row index must be between 0 and " + (width - 1) + ": " + y);
+        if (x < 0 || x >= width())
+            throw new IllegalArgumentException("column index must be between 0 and " + (width() - 1) + ": " + x);
+        if (y < 0 || y >= height())
+            throw new IllegalArgumentException("row index must be between 0 and " + (height() - 1) + ": " + y);
 
+        return Math.sqrt(yieldinX2(x,y) + yieldingY2(x,y));
     }
 
-    // sequence of indices for horizontal seam
+   /* // sequence of indices for horizontal seam
     public int[] findHorizontalSeam() {
 
     }
@@ -70,10 +72,8 @@ public class SeamCarver {
             }
         }
     }
-
-}
-
-    // remove vertical seam from current picture
+*/
+    /*// remove vertical seam from current picture
     public void removeVerticalSeam(int[] seam) {
         if (width < 1) {
             throw new IllegalArgumentException("Impossible task picture width is is less than or equal to 1.");
@@ -87,5 +87,39 @@ public class SeamCarver {
             }
         }
 
+    }*/
+
+    private int yieldinX2(int x, int y) {
+        int rgbLeft = pictureCopy.getRGB(x - 1, y);
+        int rgbRight = pictureCopy.getRGB(x + 1, y);
+        int red = (rgbRight >>16)  & 0x000000FF - (rgbLeft>> 16) & 0x000000FF ;
+        int green = (rgbRight >> 8)  & 0x000000FF - (rgbLeft >> 8)  & 0x000000FF;
+        int blue = (rgbRight)  & 0x000000FF - (rgbLeft)  & 0x000000FF;
+        return red * red + green * green + blue * blue;
+
+    }
+
+    private int yieldingY2(int x, int y) {
+        int rgbUp = pictureCopy.getRGB(x,y -1);
+        int rgbDow = pictureCopy.getRGB(x, y + 1);
+        int red = (rgbDow >> 16)  & 0x000000FF -(rgbUp >>16)  & 0x000000FF;
+        int green = (rgbDow >> 8)  & 0x000000FF -(rgbUp >> 8)  & 0x000000FF;
+        int blue = (rgbDow)  & 0x000000FF - (rgbUp)  & 0x000000FF;
+        return red * red + green * green + blue * blue;
+    }
+
+    public static void main(String[] args) {
+        Picture picture = new Picture(args[0]);
+        StdOut.printf("image is %d pixels wide by %d pixels high.\n", picture.width(), picture.height());
+
+        SeamCarver sc = new SeamCarver(picture);
+
+        StdOut.printf("Printing energy calculated for each pixel.\n");
+
+        for (int row = 0; row < sc.height(); row++) {
+            for (int col = 0; col < sc.width(); col++)
+                StdOut.printf("%9.0f ", sc.energy(col, row));
+            StdOut.println();
+        }
     }
 }
