d0da134f99a987768a25939879295ce58518650e
BrandstadterViktoria <nunviktoria@gmail.com>
2018-10-26 16:25:27 +0200

    More exercises

--- JavaLanguage/Java8Lamdas/Udemy_Tim_Buchalka_JavaMasterClass/src/TimBuchalka_LamdaChallanges.java
+++ JavaLanguage/Java8Lamdas/Udemy_Tim_Buchalka_JavaMasterClass/src/TimBuchalka_LamdaChallanges.java
@@ -1,4 +1,7 @@
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
@@ -33,6 +36,37 @@ public class TimBuchalka_LamdaChallanges {
 
         Supplier<String> iLoveJava = () -> { return "ILoveJava"; };
         System.out.println(iLoveJava.get());
+
+        List<String> topNames2015 = Arrays.asList(
+                "Amelia",
+                "Olivia",
+                "emily",
+                "Isla",
+                "Ava",
+                "oliver",
+                "Jack",
+                "Charlie",
+                "harry",
+                "Jacob"
+        );
+        topNames2015
+                .stream()
+                .map(s -> s.substring(0,1).toUpperCase() + s.substring(1))
+                .sorted()
+                .forEach(System.out::println);
+
+        topNames2015
+                .stream()
+                .map(s -> s.substring(0,1).toUpperCase() + s.substring(1))
+                .filter(s -> s.startsWith("A"))
+                .forEach(System.out::println);
+
+        topNames2015
+                .stream()
+                .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                .peek(System.out::println)
+                .sorted(String::compareTo);
+
 
 
     }
